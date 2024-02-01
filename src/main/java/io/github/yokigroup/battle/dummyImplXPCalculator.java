167ae4153b944cc700675ef9793593eb819c9a4d
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-01 19:00:36 +0100

    create new dummy implementation of XPCalculator

---
+++ src/main/java/io/github/yokigroup/battle/dummyImplXPCalculator.java
@@ -0,0 +1,15 @@
+package io.github.yokigroup.battle;
+
+import java.util.List;
+
+public class dummyImplXPCalculator extends XPCalculator {
+
+    private static final int MULTIPLIER = 100;
+    @Override
+    int getXP(List<Yokimon> defeatedOpps) {
+        if (!defeatedOpps.isEmpty()) {
+            return defeatedOpps.size() * MULTIPLIER;
+        }
+        return 0;
+    }
+}
