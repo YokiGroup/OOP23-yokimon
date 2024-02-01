f6aaab9333395aa8386e2c4dc0f9740a4e9d1dab
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-01 18:56:53 +0100

    create new dummy implementation of NextYokimon

--- src/main/java/io/github/yokigroup/battle/dummyImplNextYokimon.java
+++ src/main/java/io/github/yokigroup/battle/dummyImplNextYokimon.java
@@ -1,4 +1,15 @@
 package io.github.yokigroup.battle;
 
-public class dummyImplNextYokimon {
+import java.util.List;
+import java.util.Optional;
+
+public class dummyImplNextYokimon extends NextYokimon {
+
+    @Override
+    Optional<Yokimon> getNext(List<Yokimon> party) {
+        if (!party.isEmpty()) {
+            return Optional.of(party.get(0));
+        }
+        return Optional.empty();
+    }
 }
