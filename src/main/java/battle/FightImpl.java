bc1b7392539ee52730013c095fe5033d0f02ddc9
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-01 10:59:32 +0100

    Re-organize private fields in FightImpl class.

--- src/main/java/battle/FightImpl.java
+++ src/main/java/battle/FightImpl.java
@@ -2,30 +2,19 @@ package battle;
 
 public class FightImpl implements Fight {
 
-    private final Yokimon myYokimon;
-    private final Yokimon opponent;
-    //private final OpponentAI myOpponentAI;
+    /* parties */
+    private final List<Yokimon> myYokimons;
+    private final List<Yokimon> oppYokimons;
+    private Yokimon curr_myYokimon;
+    private Yokimon curr_oppYokimon;
+
+    /* structures */
+    private final OpponentAI oppAI = null;
+    private final XPCalculator XPCalc = null;
+    private final NextYokimon nextYok = null;
+
+    /* boolean that triggers end of fight */
     private boolean isOver;
-
-
-    /**
-     * default builder for the fight
-     * @param myYokimon my party's Yokimon involved in the fight
-     * @param opponent my Yokimon's opponent
-     */
-    public FightImpl(Yokimon myYokimon, Yokimon opponent) {
-        this.myYokimon = myYokimon;
-        this.opponent = opponent;
-    }
-
-    /**
-     * FOR DEBUG USE ONLY to try out different AIs
-     * @param e the AI that must be tested
-     */
-    public void setEnemyAI(OpponentAI e) {
-        this.myEnemyAIImpl = e;
-    }
-
 
     @Override
     public success attack(Attack myAttack) {
@@ -39,11 +28,16 @@ public class FightImpl implements Fight {
 
     @Override
     public boolean isOver() {
-        return isOver;
+        return false;
     }
 
     @Override
     public boolean victory() {
         return false;
+    }
+
+    @Override
+    public int getXP(Yokimon yokimon) {
+        return 0;
     }
 }
