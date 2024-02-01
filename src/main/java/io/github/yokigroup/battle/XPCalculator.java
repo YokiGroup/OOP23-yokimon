2e8516edfca8c2cadadeae46e6ed39204eb75683
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-01 18:27:20 +0100

    change parameter to calculate XP Points

--- src/main/java/io/github/yokigroup/battle/XPCalculator.java
+++ src/main/java/io/github/yokigroup/battle/XPCalculator.java
@@ -1,11 +1,13 @@
 package io.github.yokigroup.battle;
 
 
+import java.util.List;
+
 public abstract class XPCalculator {
 
     /**
-     * @param yokimon the yokimon who defeated an enemy and whose XP points must be updated
-     * @return the number of points that must be added
+     * @param defeatedOpps the Yokimons who have been defeated.
+     * @return the number of points that must be added to my party's current Yokimon
      */
-    abstract int getXP(Yokimon yokimon);
+    abstract int getXP(List<Yokimon> defeatedOpps);
 }
