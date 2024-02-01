5bb9201dd7333614718731bdbc758473bf35e1ce
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-01 18:41:09 +0100

    comment

--- src/main/java/io/github/yokigroup/battle/NextYokimon.java
+++ src/main/java/io/github/yokigroup/battle/NextYokimon.java
@@ -3,9 +3,11 @@ package io.github.yokigroup.battle;
 import java.util.List;
 import java.util.Optional;
 
-/**
- *
- */
 public abstract class NextYokimon {
+    /**
+     *
+     * @param party the party from which it must be chosen the next one to fight, once the current one is defeated
+     * @return the party member chosen
+     */
     abstract Optional<Yokimon> getNext(List<Yokimon> party);
 }
