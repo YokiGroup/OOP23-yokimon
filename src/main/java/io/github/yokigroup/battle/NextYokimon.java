c848d52034f83dd6798d69e9241bf61253e7963d
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-01 18:33:40 +0100

    comment

--- src/main/java/io/github/yokigroup/battle/NextYokimon.java
+++ src/main/java/io/github/yokigroup/battle/NextYokimon.java
@@ -3,6 +3,9 @@ package io.github.yokigroup.battle;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ *
+ */
 public abstract class NextYokimon {
     abstract Optional<Yokimon> getNext(List<Yokimon> party);
 }
