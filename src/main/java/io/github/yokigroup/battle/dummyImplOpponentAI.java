6a35c26ef1ccb109919d3c7cbf7112529e60f983
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-02 17:05:21 +0100

    import attack implementation

--- src/main/java/io/github/yokigroup/battle/dummyImplOpponentAI.java
+++ src/main/java/io/github/yokigroup/battle/dummyImplOpponentAI.java
@@ -4,11 +4,6 @@ public class dummyImplOpponentAI extends OpponentAI {
 
     @Override
     Attack getMove(Yokimon curr_oppYokimon) {
-        return new Attack() {
-            @Override
-            public int hashCode() {
-                return super.hashCode();
-            }
-        };
+        return null;
     }
 }
--- src/test/java/io/github/yokigroup/testImplsXPCalculator.java
+++ src/test/java/io/github/yokigroup/testImplsXPCalculator.java
@@ -1,4 +1,13 @@
 package io.github.yokigroup;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 public class testImplsXPCalculator {
+
+    @org.junit.Test //??????????
+    public void testDummy() {
+
+    }
 }
