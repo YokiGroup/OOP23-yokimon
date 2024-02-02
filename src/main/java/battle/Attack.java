ed5e286caca60cfdf96a50c96ac64608cd0080ab
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-02 23:35:10 +0100

    Add first Attack interface

---
+++ src/main/java/battle/Attack.java
@@ -0,0 +1,34 @@
+package battle;
+//import java.util.HashMap;
+public interface Attack {
+
+    public enum effect{
+        NONE
+    }
+    public enum color{
+        NONE
+    }
+    /**
+     *Return the name of the Attack
+     * @return String name
+     */
+    public String getName();
+
+    /**
+     *Return a reference of the color
+     * @return return enum color
+     */
+    public Attack.color color();
+    /**
+     *Return power of the attack (0 if it doesn't deal damage)
+     * @return integer atk pwr
+     */
+    public int attackPower();
+
+    /**
+     * Return the secondary effect of the attack
+     * @return enum effect
+     */
+    public Attack.effect getEffectID();
+
+}
