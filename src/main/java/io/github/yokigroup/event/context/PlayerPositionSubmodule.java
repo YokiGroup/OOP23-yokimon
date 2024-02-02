94d0d7ea01f5ab7578bfe81bbcee2c6fadb38b66
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-02 23:48:32 +0100

    Add skeleton of PlayerPositionSubmodule

--- src/main/java/io/github/yokigroup/event/context/PlayerPositionSubmodule.java
+++ src/main/java/io/github/yokigroup/event/context/PlayerPositionSubmodule.java
@@ -1,22 +1,34 @@
 package io.github.yokigroup.event.context;
 
 import io.github.yokigroup.util.Vector2;
+import io.github.yokigroup.world.entity.Entity;
+import io.github.yokigroup.world.tile.TileMap;
 
+/**
+ * Handles player position updates
+ */
 public class PlayerPositionSubmodule implements Submodule{
+    Entity player;
+    TileMap map;
     enum Direction{
         UP,DOWN,LEFT,RIGHT
     }
 
-    void movePlayerBy(Vector2 delta) {
-
+    public PlayerPositionSubmodule(Entity player, TileMap map){
+        this.player = player;
+        this.map = map;
     }
 
     void changeTile(Direction dir) {
+        //TODO change tile of player by going in dir
+    }
 
+    void movePlayerBy(Vector2 delta) {
+        // TODO move player by delta
     }
 
     @Override
     public void process() {
-
+        // TODO collision check
     }
 }
