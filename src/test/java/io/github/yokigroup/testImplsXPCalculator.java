22185ba9bff3526fd158d3cf98bea0d14ac12796
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-02 17:09:04 +0100

    remove implementation for XPCalculator test class

--- src/test/java/io/github/yokigroup/testImplsXPCalculator.java
+++ src/test/java/io/github/yokigroup/testImplsXPCalculator.java
@@ -5,9 +5,4 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class testImplsXPCalculator {
-
-    @org.junit.Test //??????????
-    public void testDummy() {
-
-    }
 }
