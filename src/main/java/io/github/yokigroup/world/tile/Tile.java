52635a11f9c2448ca0bd509e52fded4ebba82458
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-02 13:45:38 +0100

    Add Tile interface

--- src/main/java/io/github/yokigroup/world/tile/Tile.java
+++ src/main/java/io/github/yokigroup/world/tile/Tile.java
@@ -1,4 +1,15 @@
 package io.github.yokigroup.world.tile;
 
 public interface Tile {
+    /**
+     *
+     * @return All the hitboxes in a tile
+     */
+    Hitbox getHitboxes();
+
+    /**
+     *
+     * @return All the entities in a tile.
+     */
+    Entity getEntities();
 }
