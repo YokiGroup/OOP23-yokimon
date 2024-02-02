8acdfcb02d8142ff4998f95f34294d0433071d6d
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-02 13:46:21 +0100

    Add GameMap interface

--- src/main/java/io/github/yokigroup/world/GameMap.java
+++ src/main/java/io/github/yokigroup/world/GameMap.java
@@ -1,4 +1,12 @@
 package io.github.yokigroup.world;
 
+import io.github.yokigroup.world.tile.Tile;
+
 public interface GameMap {
+    /**
+     *
+     * @param position The world position to take the tile from.
+     * @return The tile at that position.
+     */
+    Tile getTileAt(final Vector2<Integer> position);
 }
