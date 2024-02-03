264fb8228be614d9af17125dc8c232140e0df511
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-03 17:12:05 +0100

    correct return value of OpponentAI

--- src/main/java/io/github/yokigroup/battle/OpponentAI.java
+++ src/main/java/io/github/yokigroup/battle/OpponentAI.java
@@ -1,11 +1,13 @@
 package io.github.yokigroup.battle;
 
 
+import java.util.Optional;
+
 public abstract class OpponentAI {
     /**
      *
      * @param curr_oppYokimon the Yokimon whose AI must be implemented
      * @return the most suitable attack for the opponent to use
      */
-    public abstract Attack getMove(Yokimon curr_oppYokimon);
+    public abstract Optional<Attack> getMove(Yokimon curr_oppYokimon);
 }
--- src/main/java/io/github/yokigroup/battle/dummyImplOpponentAI.java
+++ src/main/java/io/github/yokigroup/battle/dummyImplOpponentAI.java
@@ -1,14 +1,15 @@
 package io.github.yokigroup.battle;
 
+import java.util.Optional;
+
 public class dummyImplOpponentAI extends OpponentAI {
 
     /**
-     *
      * @param curr_oppYokimon the Yokimon whose AI must be implemented
      * @return the first attack on the attacks list
      */
     @Override
-    public Attack getMove(Yokimon curr_oppYokimon) {
-        return curr_oppYokimon.getAttacks().get(0);
+    public Optional<Attack> getMove(Yokimon curr_oppYokimon) {
+        return Optional.of(curr_oppYokimon.getAttacks().get(0));
     }
 }
