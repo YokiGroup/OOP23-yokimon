f4ef1fb79ed96ee1d953e09fb847e08fd6a496e9
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-03 17:46:15 +0100

    corrections

--- src/test/java/io/github/yokigroup/battleTest/OpponentAITest.java
+++ src/test/java/io/github/yokigroup/battleTest/OpponentAITest.java
@@ -11,11 +11,18 @@ public class OpponentAITest {
     @Test public void testDummyImpl() {
         OpponentAI toTest = new dummyImplOpponentAI();
 
-        Yokimon y1 = new YokimonImpl_dbg("Blue", 8, List.of());
-        assertEquals(Optional.empty(), toTest.getMove(y1));
-
         Attack a1 = new AttackImpl_dbg("Bubble", Attack.color.BLUE);
+        Attack a2 = new AttackImpl_dbg("Leaf", Attack.color.GREEN);
+
+        Yokimon y1 = new YokimonImpl_dbg("Blue", 8, List.of());
         Yokimon y2 = new YokimonImpl_dbg("Green", 3, List.of(a1));
+        Yokimon y3 = new YokimonImpl_dbg("BandG", 2, List.of(a1,a2));
+
+        //empty
+        assertEquals(Optional.empty(), toTest.getMove(y1));
+        //one attack
         assertEquals(Optional.of(a1), toTest.getMove(y2));
+        //multiple attacks
+        assertEquals(Optional.of(a1), toTest.getMove(y3));
     }
 }
