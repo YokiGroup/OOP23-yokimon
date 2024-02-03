45612e6cdfaa9888d36c66bdc3530759a00b96ed
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-03 23:08:37 +0100

    First inteface of the Altar

---
+++ src/main/java/entity/Altar.java
@@ -0,0 +1,30 @@
+package entity;
+
+import battle.Yokimon;
+
+import java.util.Optional;
+
+/**
+ * an altar which gives you a yokimon at the first interaction
+ */
+public interface Altar extends Entity{
+    public enum altarState{
+        USED,
+        NEW
+    }
+
+    /**
+     * Return the current state of the Altar (if it's used or not)
+     * @return State
+     */
+    public Altar.altarState getState();
+
+    /**
+     * return a Yokimon if the altar is new otherwise
+     * an optional empty
+     * @return Optional<Yokimon>
+     */
+    public Optional<Yokimon> getNewYokimon();
+
+
+}
