d32bcb769f7b91b424befb4bf1baa824228445d3
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-03 16:45:47 +0100

    add javadoc to dummyImplXPCalculator

--- src/main/java/io/github/yokigroup/battle/dummyImplXPCalculator.java
+++ src/main/java/io/github/yokigroup/battle/dummyImplXPCalculator.java
@@ -5,6 +5,12 @@ import java.util.List;
 public class dummyImplXPCalculator extends XPCalculator {
 
     private static final int MULTIPLIER = 100;
+
+    /**
+     *
+     * @param defeatedOpps the Yokimons who have been defeated.
+     * @return number of defeated Yokimons multiplied by a certain multiplier
+     */
     @Override
     public int getXP(List<Yokimon> defeatedOpps) {
         if (!defeatedOpps.isEmpty()) {
