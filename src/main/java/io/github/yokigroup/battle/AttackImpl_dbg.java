fbd739fde11138da7e17e50c67cbd091f356f2c9
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-03 17:31:16 +0100

    implement Attack for tests

---
+++ src/main/java/io/github/yokigroup/battle/AttackImpl_dbg.java
@@ -0,0 +1,39 @@
+package io.github.yokigroup.battle;
+
+import java.util.Objects;
+
+public class AttackImpl_dbg implements Attack {
+    private final String name;
+    private final color color;
+    private int attackPower;
+    private effect effectID;
+
+    public AttackImpl_dbg(String name, color color, int attackPower){
+        this.name = name;
+        this.color = color;
+        this.attackPower = attackPower;
+    }
+    public AttackImpl_dbg(String name, color color){
+        this(name, color, 0);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public color color() {
+        return color;
+    }
+
+    @Override
+    public int attackPower() {
+        return attackPower;
+    }
+
+    @Override
+    public effect getEffectID() {
+        return effectID;
+    }
+}
