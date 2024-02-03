8536ad22060c50682acbd140c3d0d99418fb5cac
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-03 23:07:42 +0100

    First inteface of Enemy

---
+++ src/main/java/entity/Enemy.java
@@ -0,0 +1,10 @@
+package entity;
+
+public interface Enemy extends People{
+
+    /**
+     *logic call this method when the player is
+     * to close to the player
+     */
+    public Entity.message follow();
+}
