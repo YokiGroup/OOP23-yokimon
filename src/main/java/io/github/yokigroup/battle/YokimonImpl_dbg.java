85e9907d403b1f36e3886bb20971dc09dc8345c4
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-03 16:32:18 +0100

    create Yokimon implementation for debug

---
+++ src/main/java/io/github/yokigroup/battle/YokimonImpl_dbg.java
@@ -0,0 +1,79 @@
+package io.github.yokigroup.battle;
+
+import java.util.List;
+import java.util.Map;
+
+public class YokimonImpl_dbg implements Yokimon {
+
+    private static final int BASE_XP = 0;
+    private static final int BASE_HP = 500;
+
+    private String name;
+    private boolean active = true;
+    private int level;
+    private int XP = BASE_XP;
+    private int HP = BASE_HP;
+    private List<Attack> attacks;
+
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Map<Stats, Integer> getALLStats() {
+        return null;
+    }
+
+    @Override
+    public Map<Stats, Integer> getALLBaseStats() {
+        return null;
+    }
+
+    @Override
+    public int getStat(Stats stat) {
+        return 0;
+    }
+
+    @Override
+    public int getBaseStat(BaseStats baseStat) {
+        return 0;
+    }
+
+    @Override
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public List<Attack> getAttacks() {
+        return attacks;
+    }
+
+    @Override
+    public int getActualHP() {
+        return HP;
+    }
+
+    @Override
+    public int getMAXHP() {
+        return 0;
+    }
+
+    @Override
+    public boolean levelUP(int n) {
+        return false;
+    }
+
+    @Override
+    public boolean Active() {
+        return active;
+    }
+
+    @Override
+    public exp_code takeXP(int n) {
+        XP += n;
+        return null;
+    }
+}
