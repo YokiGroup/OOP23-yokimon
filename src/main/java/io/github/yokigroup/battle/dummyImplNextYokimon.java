2e2be9ec9528dbbdfd9ce4f322743b5b32827ce6
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-03 16:46:59 +0100

    added javadoc to dummyImplNextYokimon

--- src/main/java/io/github/yokigroup/battle/dummyImplNextYokimon.java
+++ src/main/java/io/github/yokigroup/battle/dummyImplNextYokimon.java
@@ -5,6 +5,11 @@ import java.util.Optional;
 
 public class dummyImplNextYokimon extends NextYokimon {
 
+    /**
+     *
+     * @param party the party from which it must be chosen the next one to fight, once the current one is defeated
+     * @return the first element of the party list if there's any, otherwise an empty Optional
+     */
     @Override
     public Optional<Yokimon> getNext(List<Yokimon> party) {
         if (!party.isEmpty()) {
