e533cda8d484e36bb250616d397ac046321fa2b7
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-03 17:40:55 +0100

    modify import

--- src/test/java/io/github/yokigroup/battleTest/OpponentAITest.java
+++ src/test/java/io/github/yokigroup/battleTest/OpponentAITest.java
@@ -3,10 +3,7 @@ package io.github.yokigroup.battleTest;
 import java.util.List;
 import java.util.Optional;
 
-import io.github.yokigroup.battle.OpponentAI;
-import io.github.yokigroup.battle.Yokimon;
-import io.github.yokigroup.battle.YokimonImpl_dbg;
-import io.github.yokigroup.battle.dummyImplOpponentAI;
+import io.github.yokigroup.battle.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +13,9 @@ public class OpponentAITest {
 
         Yokimon y1 = new YokimonImpl_dbg("Blue", 8, List.of());
         assertEquals(Optional.empty(), toTest.getMove(y1));
+
+        Attack a1 = new AttackImpl_dbg("Bubble", Attack.color.BLUE);
+        Yokimon y2 = new YokimonImpl_dbg("Green", 3, List.of(a1));
+        assertEquals(Optional.of(a1), toTest.getMove(y2));
     }
 }
