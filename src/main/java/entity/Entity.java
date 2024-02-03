588d2e01465e03614038662fd39b6ad2815a21b9
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-03 23:06:05 +0100

    First interface of Entity object

---
+++ src/main/java/entity/Entity.java
@@ -0,0 +1,37 @@
+package entity;
+
+
+/**
+ * This class represent the concept of entity
+ */
+public interface Entity {
+    /**
+     * message for the return methods of entity and his
+     * ereditance
+     */
+    public enum message{
+        OK,
+        OVER_FLOW,
+        ERROR
+    }
+
+    /**
+     * return the current position of the entity
+     * @return Position, X e Y
+     */
+    public Position getPosition();
+
+    /**
+     * return the name of the entity, it's univocal,
+     * so it also works as an ID
+     * @return String name
+     */
+    public String getName();
+
+    public Hitbox getHitbox();
+
+    /**
+     * update the state of the entity
+     */
+    public void update();
+}
