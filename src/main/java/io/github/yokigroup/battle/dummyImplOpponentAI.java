2110d27ad19bee5a204f12843adfa10b349a48e4
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-03 16:44:14 +0100

    implement dummyImplOpponentAI

--- src/main/java/io/github/yokigroup/battle/dummyImplOpponentAI.java
+++ src/main/java/io/github/yokigroup/battle/dummyImplOpponentAI.java
@@ -2,8 +2,13 @@ package io.github.yokigroup.battle;
 
 public class dummyImplOpponentAI extends OpponentAI {
 
+    /**
+     *
+     * @param curr_oppYokimon the Yokimon whose AI must be implemented
+     * @return the first attack on the attacks list
+     */
     @Override
     public Attack getMove(Yokimon curr_oppYokimon) {
-        return null;
+        return curr_oppYokimon.getAttacks().get(0);
     }
 }
