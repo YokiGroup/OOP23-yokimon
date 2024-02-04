e91b41cbd5a1cd3f5076fbdd622884d706d9994d
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-04 22:34:25 +0100

    Update WeightedPool interface

--- src/main/java/io/github/yokigroup/util/WeightedPool.java
+++ src/main/java/io/github/yokigroup/util/WeightedPool.java
@@ -11,17 +11,29 @@ public interface WeightedPool<T> {
      * @param element The element to add to the pool.
      * @param weight The chance of an element to be chosen.
      */
-    void addRandomizedElement(final T element, final float weight);
+    void addElement(final T element, final float weight);
 
     /**
      * Removes one of the elements from the pool.
      * @param element The element to remove.
      */
-    void removeRandomizedElement(final T element);
+    void removeElement(final T element);
 
     /**
-     *
+     * Gets one of the elements in the pool by their weight without removing it.
      * @return One of the elements in the pool.
      */
     T getRandomizedElement();
+
+    /**
+     * Gets one of the elements in the pool by their weight and removes it.
+     * @return One of the elements in the pool.
+     */
+    T removeRandomizedElement();
+
+    /**
+     *
+     * @return the amount of items in the pool.
+     */
+    int size();
 }
