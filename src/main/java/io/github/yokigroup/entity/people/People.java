aa60405e30cca7c0e720269a7cf7b29a98ccd502
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-04 22:57:31 +0100

    Change the People interface to an abstract class which extendes Entity

--- src/main/java/io/github/yokigroup/entity/people/People.java
+++ src/main/java/io/github/yokigroup/entity/people/People.java
@@ -4,64 +4,114 @@ package io.github.yokigroup.entity.people;
 
 import io.github.yokigroup.battle.Yokimon;
 import io.github.yokigroup.entity.Entity;
+import io.github.yokigroup.entity.Hitbox;
 import io.github.yokigroup.entity.Position;
+import io.github.yokigroup.entity.Vector;
 
 import java.util.List;
 
 /**
  *
  */
-public interface People extends Entity {
+public abstract class People extends Entity {
+
+    private final static float DEFAULT_DIRECTION=0;
+    /**
+     * position the entity is first spawned
+     */
+    protected final Position initialPos;
+    /**
+     * direction where the entity is looking
+     */
+    protected float direction;
+
+    protected boolean Active;
+
+    protected List<Yokimon> Party;
+
+    public People(String name, Position Pos, Hitbox Hitbox, List<Yokimon> Party) {
+        super(name, Pos, Hitbox);
+        this.Party=List.copyOf(Party);
+        this.direction=DEFAULT_DIRECTION;
+        this.Active=true;
+        this.initialPos=Pos;
+
+    }
+
     /**
      * return the direction in which the player or a mpc is currently watching
+     *
      * @return the angle in radiant
      */
-    public float getDirection();
+    public float getDirection() {
+        return this.direction;
+    }
 
     /**
      * return is the person isActive or not (if is not
      * it will not perform any action)
+     *
      * @return true if is active, false if is not
      */
-    public boolean getIsActive();
+    public boolean getIsActive() {
+        return this.Active;
+    }
 
     /**
      * set is the person Active (if is not
      * it will not perform any action
      */
-    public void setActive();
+    public void setActive() {
+        this.Active=true;
+    }
 
     /**
      * set is the person not Active (if is not
      * it will not perform any action
      */
-    public void shut();
+    public void shut() {
+        this.Active=false;
+    }
 
     /**
      * return the party of the people as list of yokimon
+     *
      * @return List<Yokimon> </Yokimon>
      */
-    public List<Yokimon> getListOfYokimon();
+    public List<Yokimon> getListOfYokimon() {
+        return this.Party;
+    }
 
     /**
-     *
      * @param newYokimon add a new yokimon to the party
      * @return message
      */
-    public Entity.message addYokimon(Yokimon newYokimon);
+    public Entity.message addYokimon(Yokimon newYokimon) {
+        this.Party.add(newYokimon);
+        return message.OK;
+    }
 
     /**
-     *
      * @param newYokimons add a new list of yokimon to the party
      * @return message
      */
-    public Entity.message addListOfYokimon(List<Yokimon> newYokimons);
+    public Entity.message addListOfYokimon(List<Yokimon> newYokimons) {
+        if(this.Party.addAll(newYokimons)){
+            return message.OK;
+        }
+        else
+            return message.ERROR;
+
+    }
 
     /**
      * return the current position of the entity
+     *
      * @return Position, X e Y
      */
-    public Position getInitialPos();
+    public Position getInitialPos() {
+        return this.initialPos;
+    }
 
 }
 
