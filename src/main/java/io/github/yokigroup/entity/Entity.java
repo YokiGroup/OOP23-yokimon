ec26550aeb193c3c9400961b0a5f1b35d0550746
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-04 21:39:35 +0100

    Implementation of entity as an Abstract class

--- src/main/java/io/github/yokigroup/entity/Entity.java
+++ src/main/java/io/github/yokigroup/entity/Entity.java
@@ -4,7 +4,7 @@ package io.github.yokigroup.entity;
 /**
  * This class represent the concept of entity
  */
-public interface Entity {
+public abstract class Entity {
     /**
      * message for the return methods of entity and his
      * ereditance
@@ -12,26 +12,58 @@ public interface Entity {
     public enum message{
         OK,
         OVER_FLOW,
-        ERROR
+        ERROR,
+        OUT_OF_MAP,
+        USED
     }
+    private final String name;
+    private Position Pos;
+    private Hitbox Hitbox_type;
 
+    public Entity(String name, Position Pos, Hitbox Hitbox){
+        this.Hitbox_type=Hitbox;
+        this.name=name;
+        this.Pos=Pos;
+    }
     /**
      * return the current position of the entity
      * @return Position, X e Y
      */
-    public Position getPosition();
-
+    public Position getPosition(){
+        return this.Pos;
+    }
+    /**
+     * Set a new position for the entity, if it's valid it changes it
+     * otherwise it return an error message
+     * @return message
+     */
+    public Entity.message setPosition(Position Pos){
+        if(Pos.isValid()){
+            this.Pos=Pos;
+            return message.OK;
+        }
+        else
+            return message.OUT_OF_MAP;
+    }
     /**
      * return the name of the entity, it's univocal,
      * so it also works as an ID
      * @return String name
      */
-    public String getName();
+    public String getName(){
+        return this.name;
+    }
 
-    public Hitbox getHitbox();
+    /**
+     * return type of the Hitbox
+     * @return Hitbox
+     */
+    public Hitbox getHitbox(){
+        return this.Hitbox_type;
+    }
 
     /**
      * update the state of the entity
      */
-    public void update();
+    abstract Entity.message update();
 }
