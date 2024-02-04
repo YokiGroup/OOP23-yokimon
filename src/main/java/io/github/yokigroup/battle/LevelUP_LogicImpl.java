12b6498f263114090957b05f77990cd9cb681dde
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-05 00:13:48 +0100

    Temporary implementation of an Utility class used by Yokimon for level up and other function

---
+++ src/main/java/io/github/yokigroup/battle/LevelUP_LogicImpl.java
@@ -0,0 +1,19 @@
+package io.github.yokigroup.battle;
+
+public class LevelUP_LogicImpl implements  LevelUP_Logic{
+    private final static int DEFAULT_VALUE=0;
+    @Override
+    public int nextBoundXP(int level) {
+        return DEFAULT_VALUE;
+    }
+
+    @Override
+    public void set_Stats(Yokimon yokimon) {
+
+    }
+
+    @Override
+    public void LevelUP(Yokimon yokimon, int plus) {
+
+    }
+}
