6af9f5b1b266011a5838a06bcc777593fc3b3156
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-04 21:42:17 +0100

    Fist implementation of Altar as a class which extends Entity

--- src/main/java/io/github/yokigroup/entity/Altar.java
+++ src/main/java/io/github/yokigroup/entity/Altar.java
@@ -3,13 +3,33 @@ package io.github.yokigroup.entity;
 
 
 import io.github.yokigroup.battle.Yokimon;
+import io.github.yokigroup.entity.people.People;
 
 import java.util.Optional;
 
 /**
  * an altar which gives you a yokimon at the first interaction
  */
-public interface Altar extends Entity{
+public class Altar extends Entity{
+
+    private Optional<Yokimon> gift;
+    private altarState state;
+    public Altar(String name, Position Pos, Hitbox Hitbox, Optional<Yokimon> yokimon) {
+        super(name, Pos, Hitbox);
+        this.gift=yokimon;  //
+        this.state=altarState.NEW;
+    }
+
+    @Override
+    message update() {
+        if(this.state==altarState.NEW){
+            this.state=altarState.USED;
+            return message.OK;
+        }
+        else
+            return message.USED;
+    }
+
     public enum altarState{
         USED,
         NEW
@@ -19,14 +39,23 @@ public interface Altar extends Entity{
      * Return the current state of the Altar (if it's used or not)
      * @return State
      */
-    public Altar.altarState getState();
+    public Altar.altarState getState(){
+        return this.state;
+    }
 
     /**
      * return a Yokimon if the altar is new otherwise
      * an optional empty
      * @return Optional<Yokimon>
      */
-    public Optional<Yokimon> getNewYokimon();
+    public Optional<Yokimon> getNewYokimon(){
+        if(this.state == altarState.NEW){
+            return this.gift;
+        }
+        else
+            return Optional.empty();
+
+    }
 
 
 }
