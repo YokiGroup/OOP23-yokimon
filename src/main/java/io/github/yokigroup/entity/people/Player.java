67c7d8f8d23e7548d1620862976562d93c5772e5
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-04 22:58:33 +0100

    Change the Player interface to implemented which extendes People

--- src/main/java/io/github/yokigroup/entity/people/Player.java
+++ src/main/java/io/github/yokigroup/entity/people/Player.java
@@ -1,15 +1,32 @@
 package io.github.yokigroup.entity.people;
 
+import io.github.yokigroup.battle.Yokimon;
+import io.github.yokigroup.entity.Hitbox;
+import io.github.yokigroup.entity.Position;
 import io.github.yokigroup.entity.Vector;
-import io.github.yokigroup.entity.people.People;
 
-public interface Player extends People {
+
+import java.util.List;
+
+public class Player extends People {
+
+    public Player(String name, Position Pos, Hitbox Hitbox, List<Yokimon> Party) {
+        super(name, Pos, Hitbox, Party);
+    }
 
     /**
      * given a vector, it changes the position of the player
      * around the map
+     *
      * @param v vector
      * @return message for collision
      */
-    public message move(Vector v);
+    public message move(Vector v) {
+        return null;
+    }
+
+    @Override
+    public message update() {
+        return null;
+    }
 }
