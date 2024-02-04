035868fadd933ccd70033e91b03d37013cef1a3c
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-04 23:14:55 +0100

    Updated WeightPool with exceptions

--- src/main/java/io/github/yokigroup/util/WeightedPoolImpl.java
+++ src/main/java/io/github/yokigroup/util/WeightedPoolImpl.java
@@ -1,6 +1,7 @@
 package io.github.yokigroup.util;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
 
@@ -9,7 +10,7 @@ import java.util.Set;
  * @param <T> The type of objects contained in the pool.
  */
 public class WeightedPoolImpl<T> implements WeightedPool<T> {
-    private Set<Pair<T, Float>> itemPool;
+    private final Set<Pair<T, Float>> itemPool;
 
     public WeightedPoolImpl() {
         this.itemPool = new HashSet<>();
@@ -17,6 +18,9 @@ public class WeightedPoolImpl<T> implements WeightedPool<T> {
 
     @Override
     public void addElement(final T element, final float weight) {
+        if (weight <= 0.0f) {
+            throw new IllegalArgumentException("Weight must be positive.");
+        }
         this.itemPool.add(new Pair<>(element, weight));
     }
 
@@ -27,14 +31,23 @@ public class WeightedPoolImpl<T> implements WeightedPool<T> {
 
     @Override
     public T getRandomizedElement() {
+        if (this.size() == 0) {
+            throw new IllegalStateException("The randomized pool is empty.");
+        }
+        // Sum all the weights together
         final float sumWeight = this.itemPool.stream()
                 .map(Pair::Y)
                 .reduce(0.0f, Float::sum);
-        float randomWeight = new Random().nextFloat(sumWeight);
-        return this.itemPool.stream()
-                .reduce((a, b) -> a.Y() > randomWeight ? a : b)
-                .map(Pair::X)
-                .get();
+        // Get a randomized weight from the total
+        final float randomWeight = new Random().nextFloat(sumWeight);
+        final Optional<T> element = this.itemPool.stream()
+                // Calculate the closest weight to the randomized one.
+                .reduce((a, b) -> Math.abs(a.Y() - randomWeight) < Math.abs(b.Y() - randomWeight) ? a : b)
+                .map(Pair::X);
+        if (element.isEmpty()) {
+            throw new IllegalStateException("No element could be retrieved from the pool.");
+        }
+        return element.get();
     }
 
     @Override
