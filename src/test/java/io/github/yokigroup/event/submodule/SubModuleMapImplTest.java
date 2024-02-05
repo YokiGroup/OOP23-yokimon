78b3741a49879dc3d2ccb90188f9393ca3841ebf
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-05 22:50:16 +0100

    Add test class of SubModuleImpl

--- src/test/java/io/github/yokigroup/event/submodule/SubModuleMapImplTest.java
+++ src/test/java/io/github/yokigroup/event/submodule/SubModuleMapImplTest.java
@@ -1,23 +1,51 @@
 package io.github.yokigroup.event.submodule;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SubModuleMapImplTest {
+    private SubModuleMap map;
+    private PartySubmodule pSub;
+    private FightSubmodule fSub;
+    private Set<Submodule> subModuleTestSet;
+
+    @BeforeEach
+    void setUp() {
+        map = new SubModuleMapImpl();
+        pSub = new PartySubmodule();
+        fSub = new FightSubmodule(pSub);
+        subModuleTestSet = Set.of(pSub, fSub);
+    }
+
+    private void retrievalAsserts(){
+        assertEquals(Optional.of(pSub), map.get(pSub.getClass()));
+        assertEquals(Optional.of(fSub), map.get(fSub.getClass()));
+        assertEquals(Optional.empty(), map.get(PlayerPositionSubmodule.class));
+    }
 
     @Test
-    void register() {
+    void register_and_get() {
+        assertTrue(map.register(pSub));
+        assertTrue(map.register(fSub));
+        assertFalse(map.register(fSub));
+        retrievalAsserts();
     }
 
     @Test
     void addAll() {
-    }
-
-    @Test
-    void get() {
+        map.registerAll(subModuleTestSet);
+        retrievalAsserts();
     }
 
     @Test
     void subModuleSet() {
+        map.registerAll(subModuleTestSet);
+        assertEquals(subModuleTestSet, map.subModuleSet());
     }
 }
