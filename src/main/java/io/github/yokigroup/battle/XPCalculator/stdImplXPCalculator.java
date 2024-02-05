1328e394fe7141b1acce2fb022bd1dcd525487c2
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-05 18:15:25 +0100

    Create new implementation for XPCalculator (stdImpl)

---
+++ src/main/java/io/github/yokigroup/battle/XPCalculator/stdImplXPCalculator.java
@@ -0,0 +1,12 @@
+package io.github.yokigroup.battle.XPCalculator;
+
+import io.github.yokigroup.battle.Yokimon;
+
+import java.util.List;
+
+public class stdImplXPCalculator extends XPCalculator {
+    @Override
+    public int getXP(List<Yokimon> defeatedOpps) {
+        return 0;
+    }
+}
