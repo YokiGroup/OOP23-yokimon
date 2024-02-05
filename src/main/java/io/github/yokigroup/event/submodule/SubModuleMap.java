31dadb6242a61e292945f9f0b57248554c483cf1
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-05 20:19:07 +0100

    Change access modifier

--- src/main/java/io/github/yokigroup/event/submodule/SubModuleMap.java
+++ src/main/java/io/github/yokigroup/event/submodule/SubModuleMap.java
@@ -4,6 +4,6 @@ package io.github.yokigroup.event.submodule;
  * a SubModuleMap is an object containing zero or more (different) submodules, queryable with the provided functions
  */
 public interface SubModuleMap {
-    <T extends Submodule> void registerSubmodule(T s);
+    <T extends Submodule> boolean registerSubmodule(T s);
     <T extends Submodule> T getSubmodule(Class<T> type);
 }
