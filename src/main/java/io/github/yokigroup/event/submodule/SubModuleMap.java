2693f333a828bac1cf0177e0ce41d8b33b7fb836
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-05 20:21:40 +0100

    Add javadoc

--- src/main/java/io/github/yokigroup/event/submodule/SubModuleMap.java
+++ src/main/java/io/github/yokigroup/event/submodule/SubModuleMap.java
@@ -1,9 +1,22 @@
 package io.github.yokigroup.event.submodule;
 
+import java.util.Optional;
+
 /**
  * a SubModuleMap is an object containing zero or more (different) submodules, queryable with the provided functions
  */
 public interface SubModuleMap {
+    /**
+     * Put a submodule in the map
+     * @param s submodule to add
+     * @return true if the submodule has not been added previously, false if the added submodule overwrote another one
+     */
     <T extends Submodule> boolean registerSubmodule(T s);
-    <T extends Submodule> T getSubmodule(Class<T> type);
+
+    /**
+     * Get submodule of given class
+     * @param type Type of submodule to receive
+     * @return Optional of submodule, empty if it is not contained in the map
+     */
+    <T extends Submodule> Optional<T> getSubmodule(Class<T> type);
 }
