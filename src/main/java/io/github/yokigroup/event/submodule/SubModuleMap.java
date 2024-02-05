8f6fd232e9bf9f784b680cdd1db1da1f9316d423
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-05 20:59:44 +0100

    Add addAll method to SubModuleMaps

--- src/main/java/io/github/yokigroup/event/submodule/SubModuleMap.java
+++ src/main/java/io/github/yokigroup/event/submodule/SubModuleMap.java
@@ -12,14 +12,19 @@ public interface SubModuleMap {
      * @param s submodule to add
      * @return true if the submodule has not been added previously, false if the added submodule overwrote another one
      */
-    <T extends Submodule> boolean registerSubmodule(T s);
+    <T extends Submodule> boolean register(T s);
+
+    /**
+     *
+     */
+    <T extends Submodule> void addAll(Set<T> sSet);
 
     /**
      * Get submodule of given class
      * @param type Type of submodule to receive
      * @return Optional of submodule, empty if it is not contained in the map
      */
-    <T extends Submodule> Optional<T> getSubmodule(Class<T> type);
+    <T extends Submodule> Optional<T> get(Class<T> type);
 
     /**
      * @return Set of Submodules registered to this map
--- src/main/java/io/github/yokigroup/event/submodule/SubModuleMapImpl.java
+++ src/main/java/io/github/yokigroup/event/submodule/SubModuleMapImpl.java
@@ -10,12 +10,17 @@ public class SubModuleMapImpl implements SubModuleMap{
     private final Map<Class<? extends Submodule>, Submodule> submodules = new HashMap<>();
 
     @Override
-    public <T extends Submodule> boolean registerSubmodule(T s) {
+    public <T extends Submodule> boolean register(T s) {
         return submodules.put(s.getClass(), s) == null;
     }
 
     @Override
-    public <T extends Submodule> Optional<T> getSubmodule(Class<T> type) {
+    public <T extends Submodule> void addAll(Set<T> sSet) {
+        sSet.forEach(this::register);
+    }
+
+    @Override
+    public <T extends Submodule> Optional<T> get(Class<T> type) {
         //noinspection unchecked
         T submodule = (T) submodules.get(type);
         return submodule != null ? Optional.of(submodule) : Optional.empty();
