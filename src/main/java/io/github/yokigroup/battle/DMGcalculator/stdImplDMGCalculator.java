672bb78fe90805eac287834dfa73beacb38491b1
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-05 19:23:47 +0100

    Add javadoc

--- src/main/java/io/github/yokigroup/battle/DMGcalculator/stdImplDMGCalculator.java
+++ src/main/java/io/github/yokigroup/battle/DMGcalculator/stdImplDMGCalculator.java
@@ -3,9 +3,17 @@ package io.github.yokigroup.battle.DMGcalculator;
 import io.github.yokigroup.battle.Attack;
 import io.github.yokigroup.battle.Yokimon;
 
-public class fullImplDMGCalculator implements DMGcalculator{
+public class stdImplDMGCalculator implements DMGcalculator{
 
     private static final double MULTIPLIER = 1.2;
+
+    /**
+     * This version uses a multiplier in case a Yokimon and the attack are of the same color
+     * @param attackingYokimon the offending Yokimon
+     * @param attackedYokimon the offended Yokimon
+     * @param attack the attack used by the first one
+     * @return the actual damage (to subtract from the HP of the attacked Yokimon)
+     */
     @Override
     public int getDMG(Yokimon attackingYokimon, Yokimon attackedYokimon, Attack attack) {
         double total = (double)attackedYokimon.getStat(Yokimon.Stats.ATK)*attack.attackPower()
