c96052429fa3f6d4421a3e37b1938d3a99359b85
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-05 21:01:23 +0100

    Integrate SubModuleMap in GameOrchestrator

--- src/main/java/io/github/yokigroup/core/GameOrchestrator.java
+++ src/main/java/io/github/yokigroup/core/GameOrchestrator.java
@@ -1,41 +1,32 @@
 package io.github.yokigroup.core;
 
 import io.github.yokigroup.event.EventHandler;
-import io.github.yokigroup.event.submodule.FightSubmodule;
-import io.github.yokigroup.event.submodule.PartySubmodule;
-import io.github.yokigroup.event.submodule.PlayerPositionSubmodule;
-import io.github.yokigroup.event.submodule.Submodule;
+import io.github.yokigroup.event.submodule.*;
 import io.github.yokigroup.world.entity.Entity;
 import io.github.yokigroup.world.tile.TileMap;
 
-import java.nio.charset.IllegalCharsetNameException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 
 public class GameOrchestrator implements EventHandler {
-    private final Map<Class<? extends Submodule>, Submodule> subModules;
+    private final SubModuleMap subModules;
     private final TileMap gameMap;
     private final Entity playerCharacter;
 
     /**
      * initializes game logic submodules
      */
-    private Map<Class<? extends Submodule>, Submodule> initSubmodules(){
-        Map<Class<? extends Submodule>, Submodule> retMap = new HashMap<>();
-        Set<? extends Submodule> submodules;
+    private SubModuleMap initSubmodules(){
+        SubModuleMap retMap = new SubModuleMapImpl();
 
         // submodules this class uses
         PartySubmodule partySub = new PartySubmodule();
         PlayerPositionSubmodule playerPositionSub = new PlayerPositionSubmodule(playerCharacter, gameMap);
         FightSubmodule fightSub = new FightSubmodule(partySub);
 
-        submodules = Set.of(partySub, playerPositionSub, fightSub);
-        for(var submodule: submodules){
-            retMap.put(submodule.getClass(), submodule);
-        }
+        retMap.addAll(Set.of(partySub, playerPositionSub, fightSub));
 
         return retMap;
     }
