ae6446e2d910c98170d709a38c1ef1c0cc99fd62
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-05 15:16:19 +0100

    Implement EventHandler in GameOrchestrator

--- src/main/java/io/github/yokigroup/core/GameOrchestrator.java
+++ src/main/java/io/github/yokigroup/core/GameOrchestrator.java
@@ -1,6 +1,6 @@
 package io.github.yokigroup.core;
 
-import io.github.yokigroup.battle.Fight;
+import io.github.yokigroup.event.EventHandler;
 import io.github.yokigroup.event.submodule.FightSubmodule;
 import io.github.yokigroup.event.submodule.PartySubmodule;
 import io.github.yokigroup.event.submodule.PlayerPositionSubmodule;
@@ -8,10 +8,10 @@ import io.github.yokigroup.event.submodule.Submodule;
 import io.github.yokigroup.world.entity.Entity;
 import io.github.yokigroup.world.tile.TileMap;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
-public class GameOrchestrator {
+public class GameOrchestrator implements EventHandler {
     private final List<Submodule> subModules;
     private final TileMap gameMap;
     private final Entity playerCharacter;
@@ -25,5 +25,10 @@ public class GameOrchestrator {
         PlayerPositionSubmodule playerPositionSub = new PlayerPositionSubmodule(playerCharacter, gameMap);
         FightSubmodule fightSub = new FightSubmodule(partySub);
         subModules = List.of(partySub, playerPositionSub, fightSub);
+    }
+
+    @Override
+    public <T extends Submodule> void handle(Class<T> subModuleType, Consumer<T> handler) {
+
     }
 }
