6dce7482675492f48cb440f2adb67bd6cd1bc6d9
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-05 13:44:41 +0100

    Add empty TileShape impl

---
+++ src/main/java/io/github/yokigroup/world/gen/TileShapeImpl.java
@@ -0,0 +1,19 @@
+package io.github.yokigroup.world.gen;
+
+import io.github.yokigroup.util.WeightedPool;
+import io.github.yokigroup.world.tile.Tile;
+
+import java.util.Set;
+
+public class TileShapeImpl implements TileShape {
+
+    @Override
+    public WeightedPool<Tile> getTiles() {
+        return null;
+    }
+
+    @Override
+    public Set<Direction> getPossibleDirections() {
+        return null;
+    }
+}
