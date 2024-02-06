0b61a66eed4a3abc4fd0b0b20f5da1676109f06a
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-06 17:02:55 +0100

    Compliance of GameOrchestrator to static code analyzers

--- src/main/java/io/github/yokigroup/core/GameOrchestrator.java
+++ src/main/java/io/github/yokigroup/core/GameOrchestrator.java
@@ -1,7 +1,12 @@
 package io.github.yokigroup.core;
 
 import io.github.yokigroup.event.EventHandler;
-import io.github.yokigroup.event.submodule.*;
+import io.github.yokigroup.event.submodule.FightSubmodule;
+import io.github.yokigroup.event.submodule.PartySubmodule;
+import io.github.yokigroup.event.submodule.PlayerPositionSubmodule;
+import io.github.yokigroup.event.submodule.SubModuleMap;
+import io.github.yokigroup.event.submodule.SubModuleMapImpl;
+import io.github.yokigroup.event.submodule.Submodule;
 import io.github.yokigroup.world.entity.Entity;
 import io.github.yokigroup.world.tile.TileMap;
 
@@ -9,15 +14,20 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
 
+/**
+ * Game loop. Responsible for receiving events and updating entities
+ */
 public class GameOrchestrator implements EventHandler {
     private final SubModuleMap subModules;
     private final TileMap gameMap;
     private final Entity playerCharacter;
 
     /**
-     * initializes game logic submodules
+     * Initializes game logic submodules.
+     * @return Initialized SubModuleMap
+     * @see SubModuleMap
      */
-    private SubModuleMap initSubmodules(){
+    private SubModuleMap initSubmodules() {
         SubModuleMap retMap = new SubModuleMapImpl();
 
         // submodules this class uses
@@ -30,17 +40,20 @@ public class GameOrchestrator implements EventHandler {
         return retMap;
     }
 
+    /**
+     * Initializes a GameOrchestrator with a new TileMap and PlayerCharacter, along with the required submodules.
+     */
     public GameOrchestrator() {
-        playerCharacter = new Entity() {}; // TODO replace with Entity implementation
-        gameMap = new TileMap() {}; // TODO replace with TileMap implementation
+        playerCharacter = new Entity() { }; // TODO replace with Entity implementation
+        gameMap = new TileMap() { }; // TODO replace with TileMap implementation
         subModules = initSubmodules();
     }
 
     @Override
-    public <T extends Submodule> void handle(Class<T> subModuleType, Consumer<T> handler) {
+    public final <T extends Submodule> void handle(final Class<T> subModuleType, final Consumer<T> handler) {
         Optional<T> submodule = subModules.get(subModuleType);
-        if(submodule.isEmpty()){
-            throw new IllegalArgumentException(this.getClass()+" does not contain submodule "+subModuleType);
+        if (submodule.isEmpty()) {
+            throw new IllegalArgumentException(this.getClass() + " does not contain submodule " + subModuleType);
         }
         handler.accept(submodule.get());
     }
