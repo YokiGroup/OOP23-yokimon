f4cd75cea465922be1728183556fe26546b747e7
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-06 11:51:04 +0100

    Fix Hitbox test after split

--- src/test/java/io/github/yokigroup/entity/hitbox/HitboxTest.java
+++ src/test/java/io/github/yokigroup/entity/hitbox/HitboxTest.java
@@ -9,9 +9,9 @@ class HitboxTest {
 
     @Test
     void collidesWithCircles() {
-        final Hitbox circleHtibox1 = new HitboxImpl(new PairImpl<>(1.2f, 3.2f), 4.0f);
-        final Hitbox circleHtibox2 = new HitboxImpl(new PairImpl<>(7.2f, -1.15f), 6.0f);
-        final Hitbox circleHtibox3 = new HitboxImpl(new PairImpl<>(12.7f, 4.0f), 2.0f);
+        final Hitbox circleHtibox1 = new CircularHitbox(new PairImpl<>(1.2f, 3.2f), 4.0f);
+        final Hitbox circleHtibox2 = new CircularHitbox(new PairImpl<>(7.2f, -1.15f), 6.0f);
+        final Hitbox circleHtibox3 = new CircularHitbox(new PairImpl<>(12.7f, 4.0f), 2.0f);
         // Collisions between circles only
         assertTrue(circleHtibox1.collidesWith(circleHtibox2));
         assertTrue(circleHtibox2.collidesWith(circleHtibox1));
@@ -23,9 +23,9 @@ class HitboxTest {
 
     @Test
     void collidesWithRectangles() {
-        final Hitbox rectangleHtibox1 = new HitboxImpl(new PairImpl<>(4.2f, 5.7f), new PairImpl<>(4.7f, 4.2f));
-        final Hitbox rectangleHtibox2 = new HitboxImpl(new PairImpl<>(9.0f, 0.0f), new PairImpl<>(2.4f, 5.0f));
-        final Hitbox rectangleHtibox3 = new HitboxImpl(new PairImpl<>(5.0f, 2.0f), new PairImpl<>(10.0f, 4.0f));
+        final Hitbox rectangleHtibox1 = new RectangularHitbox(new PairImpl<>(4.2f, 5.7f), new PairImpl<>(4.7f, 4.2f));
+        final Hitbox rectangleHtibox2 = new RectangularHitbox(new PairImpl<>(9.0f, 0.0f), new PairImpl<>(2.4f, 5.0f));
+        final Hitbox rectangleHtibox3 = new RectangularHitbox(new PairImpl<>(5.0f, 2.0f), new PairImpl<>(10.0f, 4.0f));
         // Collisions between rectangles only
         assertFalse(rectangleHtibox1.collidesWith(rectangleHtibox2));
         assertFalse(rectangleHtibox2.collidesWith(rectangleHtibox1));
@@ -37,12 +37,12 @@ class HitboxTest {
 
     @Test
     void collidesWithBoth() {
-        final Hitbox circleHtibox1 = new HitboxImpl(new PairImpl<>(1.2f, 3.2f), 4.0f);
-        final Hitbox circleHtibox2 = new HitboxImpl(new PairImpl<>(7.2f, -1.15f), 6.0f);
-        final Hitbox circleHtibox3 = new HitboxImpl(new PairImpl<>(12.7f, 4.0f), 2.0f);
-        final Hitbox rectangleHtibox1 = new HitboxImpl(new PairImpl<>(4.2f, 5.7f), new PairImpl<>(4.7f, 4.2f));
-        final Hitbox rectangleHtibox2 = new HitboxImpl(new PairImpl<>(9.0f, 0.0f), new PairImpl<>(2.4f, 5.0f));
-        final Hitbox rectangleHtibox3 = new HitboxImpl(new PairImpl<>(5.0f, 2.0f), new PairImpl<>(10.0f, 4.0f));
+        final Hitbox circleHtibox1 = new CircularHitbox(new PairImpl<>(1.2f, 3.2f), 4.0f);
+        final Hitbox circleHtibox2 = new CircularHitbox(new PairImpl<>(7.2f, -1.15f), 6.0f);
+        final Hitbox circleHtibox3 = new CircularHitbox(new PairImpl<>(12.7f, 4.0f), 2.0f);
+        final Hitbox rectangleHtibox1 = new RectangularHitbox(new PairImpl<>(4.2f, 5.7f), new PairImpl<>(4.7f, 4.2f));
+        final Hitbox rectangleHtibox2 = new RectangularHitbox(new PairImpl<>(9.0f, 0.0f), new PairImpl<>(2.4f, 5.0f));
+        final Hitbox rectangleHtibox3 = new RectangularHitbox(new PairImpl<>(5.0f, 2.0f), new PairImpl<>(10.0f, 4.0f));
         // Collisions of boxes with circle1
         assertTrue(circleHtibox1.collidesWith(rectangleHtibox1));
         assertTrue(rectangleHtibox1.collidesWith(circleHtibox1));
