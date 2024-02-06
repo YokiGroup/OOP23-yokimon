cab874d49eeda1b5a839f2195de3d6bd1b88778c
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-06 11:29:52 +0100

    Merge Hitbox classes in one

--- src/main/java/io/github/yokigroup/entity/hitbox/HitboxImpl.java
+++ src/main/java/io/github/yokigroup/entity/hitbox/HitboxImpl.java
@@ -2,21 +2,56 @@ package io.github.yokigroup.entity.hitbox;
 
 import io.github.yokigroup.util.Pair;
 import io.github.yokigroup.util.PairImpl;
+import org.dyn4j.geometry.Circle;
+import org.dyn4j.geometry.Rectangle;
 import org.dyn4j.geometry.Shape;
 
 /**
  * An abstract binding to the Shape class
  */
-public abstract class HitboxImpl implements Hitbox {
+public class HitboxImpl implements Hitbox {
+    private final Shape shape;
     private Pair<Float, Float> position;
-    protected Shape shape;
 
-    public HitboxImpl(final Pair<Float, Float> position) {
-        this.position = position;
+    /**
+     * Creates a circular hitbox
+     * @param position the position of the circle
+     * @param radius the radius of the circle
+     */
+    public HitboxImpl(final Pair<Float, Float> position, final float radius) {
+        this.shape = new Circle(radius);
+        this.setPosition(position);
     }
 
-    public HitboxImpl() {
-        this(new PairImpl<>(0.0f, 0.0f));
+    /**
+     * Creates a rectangular hitbox
+     * @param position the position of the rectangle
+     * @param dimensions the width and height of the rectangle
+     */
+    public HitboxImpl(final Pair<Float, Float> position, final Pair<Float, Float> dimensions) {
+        this.shape = new Rectangle(dimensions.getX(), dimensions.getY());
+        this.setPosition(position);
+    }
+
+    /**
+     * Creates a circular hitbox at the origin
+     * @param radius the radius of the circle
+     */
+    public HitboxImpl(final float radius) {
+        this(new PairImpl<>(0.0f, 0.0f), radius);
+    }
+
+    /**
+     * Creates a rectangular hitbox at the origin
+     * @param dimensions the width and height of the rectangle
+     */
+    public HitboxImpl(final Pair<Float, Float> dimensions) {
+        this(new PairImpl<>(0.0f, 0.0f), dimensions);
+    }
+
+    @Override
+    public boolean collidesWith(final Hitbox other) {
+        return false;
     }
 
     @Override
