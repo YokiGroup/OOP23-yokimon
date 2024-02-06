db57d9a7cf6d1577697ce8752c14b2eb2cc705a4
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-06 11:02:13 +0100

    Add empty Hitbox tests

---
+++ src/test/java/io/github/yokigroup/entity/hitbox/HitboxTest.java
@@ -0,0 +1,28 @@
+package io.github.yokigroup.entity.hitbox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HitboxTest {
+
+    @Test
+    void collidesWith() {
+        
+    }
+
+    @Test
+    void setPosition() {
+
+    }
+
+    @Test
+    void getPosition() {
+
+    }
+
+    @Test
+    void getGeometry() {
+
+    }
+}
