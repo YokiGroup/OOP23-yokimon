a9650e56ec17adb9697d5bd461d98010b2295ff9
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-07 12:17:57 +0100

    Compliance of SubModuleMapImpl to static code analyzers

--- src/main/java/io/github/yokigroup/event/submodule/SubModuleMapImpl.java
+++ src/main/java/io/github/yokigroup/event/submodule/SubModuleMapImpl.java
@@ -1,33 +1,40 @@
 package io.github.yokigroup.event.submodule;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
-public class SubModuleMapImpl implements SubModuleMap{
+/**
+ * Map of submodules. Used to collect all submodules used by a game component into one place.
+ */
+public final class SubModuleMapImpl implements SubModuleMap {
     private final Map<Class<? extends Submodule>, Submodule> submodules = new HashMap<>();
 
     @Override
-    public <T extends Submodule> boolean register(T s) {
+    public <T extends Submodule> boolean register(final T s) {
         return submodules.put(s.getClass(), s) == null;
     }
 
     @Override
-    public <T extends Submodule> void registerAll(Set<T> sSet) {
+    public <T extends Submodule> void registerAll(final Set<T> sSet) {
         sSet.forEach(this::register);
     }
 
     @Override
     @SuppressWarnings("unchecked")
-    public <T extends Submodule> Optional<T> get(Class<T> type) {
+    public <T extends Submodule> Optional<T> get(final Class<T> type) {
         Submodule tempSub = submodules.get(type);
         T retSub = null;
-        if(type.isInstance(tempSub)){
-            retSub = (T)tempSub;
+        if (type.isInstance(tempSub)) {
+            retSub = (T) tempSub;
         }
         return Optional.ofNullable(retSub);
     }
 
-    public Set<Submodule> subModuleSet(){
+    @Override
+    public Set<Submodule> subModuleSet() {
         return new HashSet<>(submodules.values());
     }
 }
