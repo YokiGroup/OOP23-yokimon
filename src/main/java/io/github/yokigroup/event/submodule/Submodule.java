cb9aef8d0b7f3fe80686ea27643435db18af8aa7
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-07 13:45:14 +0100

    Compliance of Submodule to static code analyzers

--- src/main/java/io/github/yokigroup/event/submodule/Submodule.java
+++ src/main/java/io/github/yokigroup/event/submodule/Submodule.java
@@ -3,9 +3,9 @@ package io.github.yokigroup.event.submodule;
 /**
  * Submodule interface.
  * Every Submodule has the responsibility of handling and receiving events related to their function.
- * @see io.github.yokigroup.event.publisher.EventPublisherFactory.EventPublisher for how this class can subscribe to relevant events.
+ * @see io.github.yokigroup.event.EventHandler EventHandler
  */
-public interface Submodule{
+public interface Submodule {
     /**
      * to be called by the logic that integrates this submodule to let it process its inputs.
      */
