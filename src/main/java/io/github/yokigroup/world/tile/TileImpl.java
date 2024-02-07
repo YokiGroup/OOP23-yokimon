ddf15235773fa006807a27a224f7d834ceff7fa0
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-07 14:06:42 +0100

    Compliance of Tile to static code analyzers

--- src/main/java/io/github/yokigroup/world/tile/TileImpl.java
+++ src/main/java/io/github/yokigroup/world/tile/TileImpl.java
@@ -12,30 +12,54 @@ public class TileImpl implements Tile {
     private Set<Hitbox> hitboxes;
     private Set<Entity> entities;
 
+    /**
+     * Creates a tile with static and dynamic entities.
+     * @param hitboxes The invisible walls in a tile.
+     * @param entities The entities in the tile.
+     */
     public TileImpl(final Set<Hitbox> hitboxes, final Set<Entity> entities) {
         this.hitboxes = hitboxes;
         this.entities = entities;
     }
 
+    /**
+     * Creates a tile with no hitboxes and no entities.
+     */
     public TileImpl() {
         new TileImpl(Set.of(), Set.of());
     }
 
+    /**
+     *
+     * @return All the hitboxes in a tile.
+     */
     @Override
     public Set<Hitbox> getHitboxes() {
         return Set.copyOf(this.hitboxes);
     }
 
+    /**
+     *
+     * @return All the entities in a tile.
+     */
     @Override
     public Set<Entity> getEntities() {
         return Set.copyOf(this.entities);
     }
 
+    /**
+     * Adds a hitbox to the tile.
+     * @param hitbox The hitbox to add to the tile.
+     */
     @Override
     public void addHitbox(final Hitbox hitbox) {
         this.hitboxes.add(hitbox);
     }
 
+    /**
+     * Adds an entity to the tile.
+     * @param entity The entity to add to the tile.
+     */
     @Override
     public void addEntity(final Entity entity) {
         this.entities.add(entity);
--- src/test/java/io/github/yokigroup/world/tile/TileTest.java
+++ src/test/java/io/github/yokigroup/world/tile/TileTest.java
@@ -2,7 +2,7 @@ package io.github.yokigroup.world.tile;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TileTest {
 
