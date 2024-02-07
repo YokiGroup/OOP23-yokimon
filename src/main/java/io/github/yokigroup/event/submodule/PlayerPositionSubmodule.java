254dbb4f0e2a3e0a25e6ae2fa85ed9e48e0ae87f
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-07 13:38:19 +0100

    Compliance of PlayerPositionSubmodule to static code analyzers

--- src/main/java/io/github/yokigroup/event/submodule/PlayerPositionSubmodule.java
+++ src/main/java/io/github/yokigroup/event/submodule/PlayerPositionSubmodule.java
@@ -5,26 +5,59 @@ import io.github.yokigroup.world.entity.Entity;
 import io.github.yokigroup.world.tile.TileMap;
 
 /**
- * Handles player position updates
+ * Handles player position updates.
  */
-public class PlayerPositionSubmodule implements Submodule{
-    Entity player;
-    TileMap map;
-    enum Direction{
-        UP,DOWN,LEFT,RIGHT
+public class PlayerPositionSubmodule implements Submodule {
+    private final Entity player;
+    private final TileMap map;
+
+    // FIXME Replace with proper implementation
+    /**
+     * THIS WILL BE REMOVED AS SOON AS TileMap IS FINISHED.
+     */
+    public enum Direction {
+        /**
+         * Up direction.
+         */
+        UP,
+        /**
+         * Down direction.
+         */
+        DOWN,
+        /**
+         * Left direction.
+         */
+        LEFT,
+        /**
+         * Right direction.
+         */
+        RIGHT
     }
 
-    public PlayerPositionSubmodule(Entity player, TileMap map){
+    /**
+     * Instantiates a PlayerPositionSubmodule.
+     * @param player player to keep track of the position to
+     * @param map map containing player
+     */
+    public PlayerPositionSubmodule(final Entity player, final TileMap map) {
         this.player = player;
         this.map = map;
     }
 
     // TODO Change Direction reference
-    public void changeTile(Direction dir) {
+    /**
+     * attempts to change the tile of the player relative to the one it's in currently.
+     * @param dir direction to change the tile from, relative to the player's current tile
+     */
+    public void changeTile(final Direction dir) {
         //TODO change tile of player by going in dir
     }
 
-    public void movePlayerBy(Vector2 delta) {
+    /**
+     * moves player as specified by the input vector.
+     * @param delta vector to move the player by
+     */
+    public void movePlayerBy(final Vector2 delta) {
         // TODO move player by delta
     }
 
