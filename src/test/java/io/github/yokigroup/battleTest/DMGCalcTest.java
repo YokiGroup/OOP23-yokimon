3ea52d774f169a359a09f0af6f6a8ca315434757
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-07 16:43:37 +0100

    Draft for test method

--- src/test/java/io/github/yokigroup/battleTest/DMGCalcTest.java
+++ src/test/java/io/github/yokigroup/battleTest/DMGCalcTest.java
@@ -6,6 +6,10 @@ import io.github.yokigroup.battle.Yokimon;
 import io.github.yokigroup.battle.YokimonImpl;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DMGCalcTest {
 
     /* public int getDMG(Yokimon attackingYokimon, Yokimon attackedYokimon, Attack attack) {
@@ -15,12 +19,19 @@ public class DMGCalcTest {
 
     @Test public void testBasicImpl() {
 
-        Yokimon y1 = new YokimonImpl("Blue");
-        Yokimon y2 = new YokimonImpl("Red");
-        Attack a1 = new AttackImpl("Bubble", Attack.color.BLUE);
-        Attack a2 = new AttackImpl("Leaf", Attack.color.GREEN);
+        Map<Yokimon.Stats, Integer> map1 = new HashMap<>();
+        map1.put(Yokimon.Stats.ATK, 150);
+        map1.put(Yokimon.Stats.DEF, 80);
 
+        Map<Yokimon.Stats, Integer> map2 = new HashMap<>();
+        map1.put(Yokimon.Stats.ATK, 120);
+        map1.put(Yokimon.Stats.DEF, 90);
 
+        Attack a1 = new AttackImpl("Bubble", Attack.color.BLUE, 35, null);
+        Attack a2 = new AttackImpl("Leaf", Attack.color.GREEN, 55, null);
+
+        Yokimon y1 = new YokimonImpl("Squirtle", map1, null, List.of(a1));
+        Yokimon y2 = new YokimonImpl("Charizard", map2, null, List.of(a2));
 
 
     }
