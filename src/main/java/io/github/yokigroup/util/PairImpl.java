ab359c66af3644124bfc524e15d3345c4c00d0e8
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-07 16:31:43 +0100

    Change Pair JavaFX dependency

--- src/main/java/io/github/yokigroup/util/PairImpl.java
+++ src/main/java/io/github/yokigroup/util/PairImpl.java
@@ -1,12 +1,14 @@
 package io.github.yokigroup.util;
 
+import java.util.AbstractMap.SimpleEntry;
+
 /**
  * Implementation of the Pair interface binding.
  * @param <T> the type of the first element
  * @param <K> the type of the second element
  */
 public class PairImpl<T, K> implements Pair<T, K> {
-    private final javafx.util.Pair<T, K> pair;
+    private final SimpleEntry<T, K> pair;
 
     /**
      * Creates a pair with two elements.
@@ -14,7 +16,7 @@ public class PairImpl<T, K> implements Pair<T, K> {
      * @param y The second element.
      */
     public PairImpl(final T x, final K y) {
-        this.pair = new javafx.util.Pair<>(x, y);
+        this.pair = new SimpleEntry<>(x, y);
     }
 
     /**
