d7cb838ffc722c248dac7faae95912352fbc9d09
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-07 01:35:04 +0100

    Compliance of FightSubmodule to static code analyzers

--- src/main/java/io/github/yokigroup/event/submodule/FightSubmodule.java
+++ src/main/java/io/github/yokigroup/event/submodule/FightSubmodule.java
@@ -5,27 +5,41 @@ import io.github.yokigroup.battle.Fight;
 import java.util.Optional;
 
 /**
- * Handles queuing fights and starting them when the process
+ * Handles queuing fights and starting them when the process.
+ * @author Giovanni Paone
  */
-public class FightSubmodule implements Submodule{
-    Optional<Fight> lastAnnouncedFight = Optional.empty();
-    PartySubmodule party; // keeping a reference to the party submodule to get a list of yokimons whenever necessary
+public class FightSubmodule implements Submodule {
+    private Optional<Fight> lastAnnouncedFight = Optional.empty();
+    private final PartySubmodule party; // keeping a reference to the party submodule to get a list of yokimons whenever necessary
 
-    public FightSubmodule(PartySubmodule party){
+    /**
+     * Instantiate a FightSubmodule.
+     * @param party party submodule reference to consult when fights start
+     */
+    public FightSubmodule(final PartySubmodule party) {
         this.party = party;
     }
 
-    public void addEncounter(Fight f){
+    /**
+     * Adds Fight {@param f} to be processed as next encounter.
+     * @param f
+     */
+    public void addEncounter(final Fight f) {
+        // FIXME perhaps Fight could adopt the Builder design pattern?
         lastAnnouncedFight = Optional.ofNullable(f);
     }
 
-    public Optional<Fight> getLastAnnouncedFight(){
+    /**
+     * Get last Fight added and not yet processed by the game logic.
+     * @return last Fight as detailed above, if any
+     */
+    public Optional<Fight> getLastAnnouncedFight() {
         return lastAnnouncedFight;
     }
 
     @Override
-    public void process() {
-        if(lastAnnouncedFight.isPresent()){
+    public final void process() {
+        if (lastAnnouncedFight.isPresent()) {
             // TODO start fight
         }
     }
