d2a54591a5d8cb50f155b27f04f621fc719efd5c
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-08 17:58:41 +0100

    Compliance of DmgCalculator to static code analyzers

--- src/main/java/io/github/yokigroup/battle/DmgCalculator/DmgCalculator.java
+++ src/main/java/io/github/yokigroup/battle/DmgCalculator/DmgCalculator.java
@@ -3,10 +3,13 @@ package io.github.yokigroup.battle.DmgCalculator;
 import io.github.yokigroup.battle.Attack;
 import io.github.yokigroup.battle.Yokimon;
 
+/**
+ * Interface for Damage Calculator
+ */
 public interface DmgCalculator {
 
     /**
-     * Calculates the actual damage provided by the attack
+     * Calculates the actual damage provided by the attack.
      * @param attackingYokimon the offending Yokimon
      * @param attackedYokimon the offended Yokimon
      * @param attack the attack used by the first one
