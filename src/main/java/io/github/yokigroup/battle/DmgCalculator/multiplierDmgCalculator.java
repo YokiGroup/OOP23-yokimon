ff1fa8118ff5a709bbed326b25c2ada9ede02e64
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-08 18:05:01 +0100

    Compliance of multiplierDmgCalculator to static code analyzers

--- src/main/java/io/github/yokigroup/battle/DmgCalculator/multiplierDmgCalculator.java
+++ src/main/java/io/github/yokigroup/battle/DmgCalculator/multiplierDmgCalculator.java
@@ -3,20 +3,24 @@ package io.github.yokigroup.battle.DmgCalculator;
 import io.github.yokigroup.battle.Attack;
 import io.github.yokigroup.battle.Yokimon;
 
+/**
+ * Fuller version of Damage Calculator, that takes into consideration
+ * the two Yokimons' color and the attack color.
+ */
 public class multiplierDmgCalculator implements DmgCalculator {
 
     private static final double MULTIPLIER = 1.2;
 
     /**
-     * This version uses a multiplier in case a Yokimon and the attack are of the same color
+     * This version uses a multiplier in case a Yokimon and the attack are of the same color.
      * @param attackingYokimon the offending Yokimon
      * @param attackedYokimon the offended Yokimon
      * @param attack the attack used by the first one
      * @return the actual damage (to subtract from the HP of the attacked Yokimon)
      */
     @Override
-    public int getDMG(Yokimon attackingYokimon, Yokimon attackedYokimon, Attack attack) {
-        double total = (double)(attackingYokimon.getStat(Yokimon.Stats.ATK) * attack.attackPower()
+    public int getDMG(final Yokimon attackingYokimon, final Yokimon attackedYokimon, final Attack attack) {
+        double total = (double) (attackingYokimon.getStat(Yokimon.Stats.ATK) * attack.attackPower()
                 / attackedYokimon.getStat(Yokimon.Stats.DEF));
 
         if (attackingYokimon.getColor().equals(attack.color())) {
@@ -26,6 +30,6 @@ public class multiplierDmgCalculator implements DmgCalculator {
             total = total / MULTIPLIER;
         }
 
-        return (int)total;
+        return (int) total;
     }
 }
