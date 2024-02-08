7d9986ffef7ec69940cf3d2a323c117408b276ba
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-08 17:49:17 +0100

    New public Enum Color use substitute colors from Attack interface

---
+++ src/main/java/io/github/yokigroup/battle/Color.java
@@ -0,0 +1,18 @@
+package io.github.yokigroup.battle;
+
+/**
+ * This enum represents the different colors that yokimon can take on
+ * or the color which an attack will have
+ * A yokimon which attack with a move of the same color of himself deal more damage
+ * According to the compatibility of the colors:
+ * purple -> deal less damage to black but more to red \
+ * red -> deal less purple damage to  but more to black \
+ * black -> deal less red damage to  but more to purple \
+ * white -> has no damage modifier
+ */
+public enum Color {
+    purple,
+    red,
+    black,
+    white
+}
