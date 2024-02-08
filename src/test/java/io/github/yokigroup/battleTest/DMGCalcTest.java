40d7a43dae53404c007b74108abf289609aa37b2
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-08 14:26:53 +0100

    Correct test for basicDMGCalculator

--- src/test/java/io/github/yokigroup/battleTest/DMGCalcTest.java
+++ src/test/java/io/github/yokigroup/battleTest/DMGCalcTest.java
@@ -1,10 +1,7 @@
 package io.github.yokigroup.battleTest;
 
-import io.github.yokigroup.battle.Attack;
-import io.github.yokigroup.battle.AttackImpl;
+import io.github.yokigroup.battle.*;
 import io.github.yokigroup.battle.DMGcalculator.*;
-import io.github.yokigroup.battle.Yokimon;
-import io.github.yokigroup.battle.YokimonImpl;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -15,10 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DMGCalcTest {
 
-    /* public int getDMG(Yokimon attackingYokimon, Yokimon attackedYokimon, Attack attack) {
-        return (int)(ATK)*Power()      /attackedYokimon.getStat(DEF);
-    } */
-
     @Test public void testBasicImpl() {
         DMGCalculator toTest = new basicImplDMGCalculator();
 
@@ -27,15 +20,23 @@ public class DMGCalcTest {
         map1.put(Yokimon.Stats.DEF, 80);
 
         Map<Yokimon.Stats, Integer> map2 = new HashMap<>();
-        map1.put(Yokimon.Stats.ATK, 120);
-        map1.put(Yokimon.Stats.DEF, 90);
+        map2.put(Yokimon.Stats.ATK, 120);
+        map2.put(Yokimon.Stats.DEF, 90);
 
         Attack a1 = new AttackImpl("Bubble", Attack.color.BLUE, 35, null);
         Attack a2 = new AttackImpl("Leaf", Attack.color.GREEN, 55, null);
 
-        Yokimon y1 = new YokimonImpl("Squirtle", map1, null, List.of(a1));
-        Yokimon y2 = new YokimonImpl("Charizard", map2, null, List.of(a2));
 
+        Yokimon y1 = new YokimonBuilder("Caterpie", Attack.color.GREEN, map1, YokimonImpl.DEFAULT_GROWRATE)
+                .stats(map1)
+                .build();
+
+        Yokimon y2 = new YokimonBuilder("Charizard", Attack.color.RED, map2, YokimonImpl.DEFAULT_GROWRATE)
+                .stats(map2)
+                .build();
+
+        assert(!map1.isEmpty());
+        assert (!map2.isEmpty());
         assertEquals(59, toTest.getDMG(y1,y2,a1));
     }
 }
