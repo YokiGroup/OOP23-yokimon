4da37002acefd54c8f4fdec4a4365fd40430e8da
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-08 13:14:32 +0100

    Create new Builder implementation for YokimonImpl

---
+++ src/main/java/io/github/yokigroup/battle/YokimonBuilderImpl.java
@@ -0,0 +1,68 @@
+package io.github.yokigroup.battle;
+
+public class YokimonBuilderImpl implements YokimonBuilder {
+    @Override
+    public void setName() {
+
+    }
+
+    @Override
+    public void setColor() {
+
+    }
+
+    @Override
+    public void setBaseStats() {
+
+    }
+
+    @Override
+    public void setStats() {
+
+    }
+
+    @Override
+    public void setLevel() {
+
+    }
+
+    @Override
+    public void setMAX_HP() {
+
+    }
+
+    @Override
+    public void setACTUAL_HP() {
+
+    }
+
+    @Override
+    public void setGrowRate() {
+
+    }
+
+    @Override
+    public void setXP() {
+
+    }
+
+    @Override
+    public void setXP_NEXTLEV() {
+
+    }
+
+    @Override
+    public void setMoves() {
+
+    }
+
+    @Override
+    public void setActive() {
+
+    }
+
+    @Override
+    public Yokimon getYokimon() {
+        return null;
+    }
+}
