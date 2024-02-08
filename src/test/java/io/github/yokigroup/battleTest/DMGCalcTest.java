ff1141da1fc63253d5ce17f3602a0cf863cd53fa
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-08 15:29:00 +0100

    Correct test class

--- src/test/java/io/github/yokigroup/battleTest/DMGCalcTest.java
+++ src/test/java/io/github/yokigroup/battleTest/DMGCalcTest.java
@@ -24,6 +24,7 @@ public class DMGCalcTest {
         map2.put(Yokimon.Stats.DEF, 90);
 
         Attack a1 = new AttackImpl("Bubble", Attack.color.BLUE, 35, null);
+        Attack a2 = new AttackImpl("Leaf", Attack.color.GREEN, 55, null);
 
         Yokimon y1 = new YokimonImpl("Caterpie", Attack.color.GREEN, map1, map1, YokimonImpl.DEFAULT_LEVEL,
                             YokimonImpl.DEFAULT_GROWRATE, Collections.emptyList(), true);
@@ -32,6 +33,32 @@ public class DMGCalcTest {
                 YokimonImpl.DEFAULT_GROWRATE, Collections.emptyList(), true);
 
         assertEquals(58, toTest.getDMG(y1,y2,a1));
+        assertEquals(91, toTest.getDMG(y1,y2,a2));
+
+    }
+
+    @Test public void testMultiplierImpl() {
+        DMGCalculator toTest = new multiplierDMGCalculator();
+
+        Map<Yokimon.Stats, Integer> map1 = new HashMap<>();
+        map1.put(Yokimon.Stats.ATK, 150);
+        map1.put(Yokimon.Stats.DEF, 80);
+
+        Map<Yokimon.Stats, Integer> map2 = new HashMap<>();
+        map2.put(Yokimon.Stats.ATK, 120);
+        map2.put(Yokimon.Stats.DEF, 90);
+
+        Attack a1 = new AttackImpl("Bubble", Attack.color.BLUE, 35, null);
+        Attack a2 = new AttackImpl("Leaf", Attack.color.GREEN, 55, null);
+
+        Yokimon y1 = new YokimonImpl("Caterpie", Attack.color.GREEN, map1, map1, YokimonImpl.DEFAULT_LEVEL,
+                YokimonImpl.DEFAULT_GROWRATE, Collections.emptyList(), true);
+
+        Yokimon y2 = new YokimonImpl("Squirtle", Attack.color.BLUE, map2, map2, YokimonImpl.DEFAULT_LEVEL,
+                YokimonImpl.DEFAULT_GROWRATE, Collections.emptyList(), true);
+
+        assertEquals(48, toTest.getDMG(y1,y2,a1));
+        assertEquals(109, toTest.getDMG(y1,y2,a2));
 
     }
 }
