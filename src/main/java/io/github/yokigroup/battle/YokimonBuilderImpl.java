21a03374685aa354fd215d5c18fbbaa33a067413
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-08 13:21:16 +0100

    Insert parameters for Yokimon Builder methods

--- src/main/java/io/github/yokigroup/battle/YokimonBuilderImpl.java
+++ src/main/java/io/github/yokigroup/battle/YokimonBuilderImpl.java
@@ -1,68 +1,8 @@
 package io.github.yokigroup.battle;
 
-public class YokimonBuilderImpl implements YokimonBuilder {
-    @Override
-    public void setName() {
+import java.util.List;
+import java.util.Map;
 
-    }
+public class YokimonBuilderImpl{
 
-    @Override
-    public void setColor() {
-
-    }
-
-    @Override
-    public void setBaseStats() {
-
-    }
-
-    @Override
-    public void setStats() {
-
-    }
-
-    @Override
-    public void setLevel() {
-
-    }
-
-    @Override
-    public void setMAX_HP() {
-
-    }
-
-    @Override
-    public void setACTUAL_HP() {
-
-    }
-
-    @Override
-    public void setGrowRate() {
-
-    }
-
-    @Override
-    public void setXP() {
-
-    }
-
-    @Override
-    public void setXP_NEXTLEV() {
-
-    }
-
-    @Override
-    public void setMoves() {
-
-    }
-
-    @Override
-    public void setActive() {
-
-    }
-
-    @Override
-    public Yokimon getYokimon() {
-        return null;
-    }
 }
