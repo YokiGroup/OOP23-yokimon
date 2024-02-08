bac165122bbd72c7bc40e8cf596653d0c33f6cba
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-08 15:06:25 +0100

    Correct basicImplDMGCalculator class

--- src/main/java/io/github/yokigroup/battle/DMGcalculator/basicImplDMGCalculator.java
+++ src/main/java/io/github/yokigroup/battle/DMGcalculator/basicImplDMGCalculator.java
@@ -6,7 +6,7 @@ import io.github.yokigroup.battle.Yokimon;
 public class basicImplDMGCalculator implements DMGCalculator {
     @Override
     public int getDMG(Yokimon attackingYokimon, Yokimon attackedYokimon, Attack attack) {
-        return (int)attackedYokimon.getStat(Yokimon.Stats.ATK)*attack.attackPower()
-                /attackedYokimon.getStat(Yokimon.Stats.DEF);
+        return (int)(attackingYokimon.getStat(Yokimon.Stats.ATK)*attack.attackPower()
+                /attackedYokimon.getStat(Yokimon.Stats.DEF));
     }
 }
