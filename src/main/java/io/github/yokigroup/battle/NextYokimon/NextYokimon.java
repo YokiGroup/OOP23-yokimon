63c35afdeb3175ceec79b6e0bfb147d523752f4e
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-08 18:28:53 +0100

    Compliance of NextYokimon to static code analyzers

--- src/main/java/io/github/yokigroup/battle/NextYokimon/NextYokimon.java
+++ src/main/java/io/github/yokigroup/battle/NextYokimon/NextYokimon.java
@@ -5,9 +5,12 @@ import io.github.yokigroup.battle.Yokimon;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * This is meant to select the best next Yokimon to use in the fight, once the current one is defeated.
+ * The choice is based on different criteria, based on the different implementations.
+ */
 public abstract class NextYokimon {
     /**
-     *
      * @param party the party from which it must be chosen the next one to fight, once the current one is defeated
      * @return the party member chosen
      */
