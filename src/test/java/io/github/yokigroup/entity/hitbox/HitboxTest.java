b50f0c61ce71dc462dd2ba16ec18620c8113ca3c
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-09 11:00:20 +0100

    Add BeforeEach to HitboxTest

--- src/test/java/io/github/yokigroup/entity/hitbox/HitboxTest.java
+++ src/test/java/io/github/yokigroup/entity/hitbox/HitboxTest.java
@@ -4,18 +4,33 @@ import io.github.yokigroup.util.Vector2Impl;
 import io.github.yokigroup.world.entity.hitbox.CircularHitbox;
 import io.github.yokigroup.world.entity.hitbox.Hitbox;
 import io.github.yokigroup.world.entity.hitbox.RectangularHitbox;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class HitboxTest {
+    private static Hitbox circleHtibox1;
+    private static Hitbox circleHtibox2;
+    private static Hitbox circleHtibox3;
+    private static Hitbox rectangleHtibox1;
+    private static Hitbox rectangleHtibox2;
+    private static Hitbox rectangleHtibox3;
+
+    @BeforeEach
+    public void init() {
+        circleHtibox1 = new CircularHitbox(new Vector2Impl(1.2d, 3.2d), 4.0d);
+        circleHtibox2 = new CircularHitbox(new Vector2Impl(7.2d, -1.15d), 6.0d);
+        circleHtibox3 = new CircularHitbox(new Vector2Impl(12.7d, 4.0d), 2.0d);
+
+        rectangleHtibox1 = new RectangularHitbox(new Vector2Impl(4.2d, 5.7d), new Vector2Impl(4.7d, 4.2d));
+        rectangleHtibox2 = new RectangularHitbox(new Vector2Impl(9.0d, 0.0d), new Vector2Impl(2.4d, 5.0d));
+        rectangleHtibox3 = new RectangularHitbox(new Vector2Impl(5.0d, 2.0d), new Vector2Impl(10.0d, 4.0d));
+    }
 
     @Test
     void collidesWithCircles() {
-        final Hitbox circleHtibox1 = new CircularHitbox(new Vector2Impl(1.2d, 3.2d), 4.0d);
-        final Hitbox circleHtibox2 = new CircularHitbox(new Vector2Impl(7.2d, -1.15d), 6.0d);
-        final Hitbox circleHtibox3 = new CircularHitbox(new Vector2Impl(12.7d, 4.0d), 2.0d);
         // Collisions between circles only
         assertTrue(circleHtibox1.collidesWith(circleHtibox2));
         assertTrue(circleHtibox2.collidesWith(circleHtibox1));
@@ -27,9 +42,6 @@ class HitboxTest {
 
     @Test
     void collidesWithRectangles() {
-        final Hitbox rectangleHtibox1 = new RectangularHitbox(new Vector2Impl(4.2d, 5.7d), new Vector2Impl(4.7d, 4.2d));
-        final Hitbox rectangleHtibox2 = new RectangularHitbox(new Vector2Impl(9.0d, 0.0d), new Vector2Impl(2.4d, 5.0d));
-        final Hitbox rectangleHtibox3 = new RectangularHitbox(new Vector2Impl(5.0d, 2.0d), new Vector2Impl(10.0d, 4.0d));
         // Collisions between rectangles only
         assertFalse(rectangleHtibox1.collidesWith(rectangleHtibox2));
         assertFalse(rectangleHtibox2.collidesWith(rectangleHtibox1));
@@ -41,12 +53,6 @@ class HitboxTest {
 
     @Test
     void collidesWithBoth() {
-        final Hitbox circleHtibox1 = new CircularHitbox(new Vector2Impl(1.2d, 3.2d), 4.0d);
-        final Hitbox circleHtibox2 = new CircularHitbox(new Vector2Impl(7.2d, -1.15d), 6.0d);
-        final Hitbox circleHtibox3 = new CircularHitbox(new Vector2Impl(12.7d, 4.0d), 2.0d);
-        final Hitbox rectangleHtibox1 = new RectangularHitbox(new Vector2Impl(4.2d, 5.7d), new Vector2Impl(4.7d, 4.2d));
-        final Hitbox rectangleHtibox2 = new RectangularHitbox(new Vector2Impl(9.0d, 0.0d), new Vector2Impl(2.4d, 5.0d));
-        final Hitbox rectangleHtibox3 = new RectangularHitbox(new Vector2Impl(5.0d, 2.0d), new Vector2Impl(10.0d, 4.0d));
         // Collisions of boxes with circle1
         assertTrue(circleHtibox1.collidesWith(rectangleHtibox1));
         assertTrue(rectangleHtibox1.collidesWith(circleHtibox1));
