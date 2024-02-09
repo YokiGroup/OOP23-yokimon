d5175e3e37335334ba3f8713b7874a72cd82fbd2
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-09 11:02:17 +0100

    Add BeforeEach to WeightedPoolTest

--- src/test/java/io/github/yokigroup/util/WeightedPoolTest.java
+++ src/test/java/io/github/yokigroup/util/WeightedPoolTest.java
@@ -1,5 +1,6 @@
 package io.github.yokigroup.util;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -12,12 +13,17 @@ class WeightedPoolTest {
     private final float weight2 = 0.5f;
     private final float weight3 = 0.75f;
     private final float weight4 = 1.0f;
+    private WeightedPool<String> pool;
+
+    @BeforeEach
+    public void init() {
+        pool = new WeightedPoolImpl<>();
+        pool.addElement("elt1", weight1);
+        pool.addElement("elt2", weight2);
+    }
 
     @Test
     void addRandomizedElement() {
-        final WeightedPool<String> pool = new WeightedPoolImpl<>();
-        pool.addElement("elt1", weight1);
-        pool.addElement("elt2", weight2);
         pool.addElement("elt3", weight3);
         assertEquals(3, pool.size());
         pool.addElement("elt4", weight4);
@@ -26,9 +32,6 @@ class WeightedPoolTest {
 
     @Test
     void removeElement() {
-        final WeightedPool<String> pool = new WeightedPoolImpl<>();
-        pool.addElement("elt1", weight1);
-        pool.addElement("elt2", weight2);
         pool.addElement("elt3", weight3);
         assertEquals(3, pool.size());
         pool.removeElement("elt1");
@@ -41,9 +44,6 @@ class WeightedPoolTest {
 
     @Test
     void getRandomizedElement() {
-        final WeightedPool<String> pool = new WeightedPoolImpl<>();
-        pool.addElement("elt1", weight1);
-        pool.addElement("elt2", weight2);
         assertTrue(List.of("elt1", "elt2").contains(pool.getRandomizedElement()));
         assertTrue(List.of("elt1", "elt2").contains(pool.getRandomizedElement()));
         assertTrue(List.of("elt1", "elt2").contains(pool.getRandomizedElement()));
@@ -54,9 +54,6 @@ class WeightedPoolTest {
 
     @Test
     void removeRandomizedElement() {
-        final WeightedPool<String> pool = new WeightedPoolImpl<>();
-        pool.addElement("elt1", weight1);
-        pool.addElement("elt2", weight2);
         assertEquals(2, pool.size());
         assertTrue(List.of("elt1", "elt2").contains(pool.removeRandomizedElement()));
         assertEquals(1, pool.size());
