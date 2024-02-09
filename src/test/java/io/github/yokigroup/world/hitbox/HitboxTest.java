41059e942d771d43296fb17a16f9395d3a228025
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-09 11:23:43 +0100

    Compliance of HitboxTest to static code analyzers

--- src/test/java/io/github/yokigroup/world/hitbox/HitboxTest.java
+++ src/test/java/io/github/yokigroup/world/hitbox/HitboxTest.java
@@ -20,13 +20,33 @@ class HitboxTest {
 
     @BeforeEach
     public void init() {
-        circleHtibox1 = new CircularHitbox(new Vector2Impl(1.2d, 3.2d), 4.0d);
-        circleHtibox2 = new CircularHitbox(new Vector2Impl(7.2d, -1.15d), 6.0d);
-        circleHtibox3 = new CircularHitbox(new Vector2Impl(12.7d, 4.0d), 2.0d);
-
-        rectangleHtibox1 = new RectangularHitbox(new Vector2Impl(4.2d, 5.7d), new Vector2Impl(4.7d, 4.2d));
-        rectangleHtibox2 = new RectangularHitbox(new Vector2Impl(9.0d, 0.0d), new Vector2Impl(2.4d, 5.0d));
-        rectangleHtibox3 = new RectangularHitbox(new Vector2Impl(5.0d, 2.0d), new Vector2Impl(10.0d, 4.0d));
+        final double x1 = 1.2d;
+        final double y1 = 3.2d;
+        final double x2 = 7.2d;
+        final double y2 = -1.15d;
+        final double x3 = 12.7d;
+        final double y3 = 4.0d;
+        final double r1 = 4.0d;
+        final double r2 = 6.0d;
+        final double r3 = 2.0d;
+        final double x4 = 4.2d;
+        final double y4 = 5.7d;
+        final double x5 = 9.0d;
+        final double y5 = 0.0d;
+        final double x6 = 5.0d;
+        final double y6 = 2.0d;
+        final double w1 = 4.7d;
+        final double h1 = 4.2d;
+        final double w2 = 2.4d;
+        final double h2 = 5.0d;
+        final double w3 = 10.0d;
+        final double h3 = 4.0d;
+        circleHtibox1 = new CircularHitbox(new Vector2Impl(x1, y1), r1);
+        circleHtibox2 = new CircularHitbox(new Vector2Impl(x2, y2), r2);
+        circleHtibox3 = new CircularHitbox(new Vector2Impl(x3, y3), r3);
+        rectangleHtibox1 = new RectangularHitbox(new Vector2Impl(x4, y4), new Vector2Impl(w1, h1));
+        rectangleHtibox2 = new RectangularHitbox(new Vector2Impl(x5, y5), new Vector2Impl(w2, h2));
+        rectangleHtibox3 = new RectangularHitbox(new Vector2Impl(x6, y6), new Vector2Impl(w3, h3));
     }
 
     @Test
