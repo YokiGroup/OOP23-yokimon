00fdf7965b07a4fb092a8674981f53ebd9140c05
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-09 11:18:44 +0100

    Compliance of XPCalcTest to static code analyzers

--- src/test/java/io/github/yokigroup/battleTest/XPCalcTest.java
+++ src/test/java/io/github/yokigroup/battleTest/XPCalcTest.java
@@ -6,9 +6,21 @@ import io.github.yokigroup.battle.Yokimon;
 import io.github.yokigroup.battle.YokimonImpl;
 import io.github.yokigroup.battle.xpcalculator.DummyImplXPCalculator;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+/**
+ * Test class for XP Calculator.
+ */
 public class XPCalcTest {
+
+    private static final int EXP_VAL_DUMMY1 = 0;
+    private static final int EXP_VAL_DUMMY2 = 300;
+    private static final int EXP_VAL_DUMMY3 = 500;
+
+    /**
+     * Testing dummy implementation.
+     */
     @Test public void testDummyImpl() {
         XPCalculator toTest = new DummyImplXPCalculator();
 
@@ -16,9 +28,9 @@ public class XPCalcTest {
         Yokimon y2 = new YokimonImpl("Red");
         Yokimon y3 = new YokimonImpl("Yellow");
 
-        assertEquals(0, toTest.getXP(List.of()));
-        assertEquals(300, toTest.getXP(List.of(y1,y2,y3)));
-        assertNotEquals(500, toTest.getXP(List.of(y1,y2)));
+        assertEquals(EXP_VAL_DUMMY1, toTest.getXP(List.of()));
+        assertEquals(EXP_VAL_DUMMY2, toTest.getXP(List.of(y1, y2, y3)));
+        assertNotEquals(EXP_VAL_DUMMY3, toTest.getXP(List.of(y1, y2)));
 
     }
 }
