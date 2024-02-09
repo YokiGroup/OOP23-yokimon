2bf64b7d314e6e7813b5af7fb7eb2bd3ad1f8700
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-09 11:19:24 +0100

    Compliance of DMGCalcTest to static code analyzers

--- src/test/java/io/github/yokigroup/battleTest/DMGCalcTest.java
+++ src/test/java/io/github/yokigroup/battleTest/DMGCalcTest.java
@@ -1,14 +1,20 @@
 package io.github.yokigroup.battleTest;
 
-import io.github.yokigroup.battle.*;
-import io.github.yokigroup.battle.dmgcalculator.*;
-import org.junit.jupiter.api.BeforeAll;
+import io.github.yokigroup.battle.Attack;
+import io.github.yokigroup.battle.AttackImpl;
+import io.github.yokigroup.battle.Yokimon;
+import io.github.yokigroup.battle.dmgcalculator.BasicImplDmgCalculator;
+import io.github.yokigroup.battle.dmgcalculator.DmgCalculator;
+import io.github.yokigroup.battle.dmgcalculator.MultiplierDmgCalculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
 
 import io.github.yokigroup.battle.YokimonImpl;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,6 +31,8 @@ public class DMGCalcTest {
     private static final int DEF_VALUE1 = 80;
     private static final int ATK_VALUE2 = 120;
     private static final int DEF_VALUE2 = 90;
+    private static final int PWR_VALUE1 = 35;
+    private static final int PWR_VALUE2 = 55;
     private static Map<Yokimon.Stats, Integer> map1;
     private static Map<Yokimon.Stats, Integer> map2;
 
@@ -50,8 +58,8 @@ public class DMGCalcTest {
     @Test public void testBasicImpl() {
         DmgCalculator toTest = new BasicImplDmgCalculator();
 
-        Attack a1 = new AttackImpl("Bubble", Attack.color.BLUE, 35, null);
-        Attack a2 = new AttackImpl("Leaf", Attack.color.GREEN, 55, null);
+        Attack a1 = new AttackImpl("Bubble", Attack.color.BLUE, PWR_VALUE1, null);
+        Attack a2 = new AttackImpl("Leaf", Attack.color.GREEN, PWR_VALUE2, null);
 
         Yokimon y1 = new YokimonImpl("Caterpie", Attack.color.GREEN, map1, map1, YokimonImpl.DEFAULT_LEVEL,
                             YokimonImpl.DEFAULT_GROWRATE, Collections.emptyList(), true);
@@ -70,16 +78,8 @@ public class DMGCalcTest {
     @Test public void testMultiplierImpl() {
         DmgCalculator toTest = new MultiplierDmgCalculator();
 
-        Map<Yokimon.Stats, Integer> map1 = new HashMap<>();
-        map1.put(Yokimon.Stats.ATK, 150);
-        map1.put(Yokimon.Stats.DEF, 80);
-
-        Map<Yokimon.Stats, Integer> map2 = new HashMap<>();
-        map2.put(Yokimon.Stats.ATK, 120);
-        map2.put(Yokimon.Stats.DEF, 90);
-
-        Attack a1 = new AttackImpl("Bubble", Attack.color.BLUE, 35, null);
-        Attack a2 = new AttackImpl("Leaf", Attack.color.GREEN, 55, null);
+        Attack a1 = new AttackImpl("Bubble", Attack.color.BLUE, PWR_VALUE1, null);
+        Attack a2 = new AttackImpl("Leaf", Attack.color.GREEN, PWR_VALUE2, null);
 
         Yokimon y1 = new YokimonImpl("Caterpie", Attack.color.GREEN, map1, map1, YokimonImpl.DEFAULT_LEVEL,
                 YokimonImpl.DEFAULT_GROWRATE, Collections.emptyList(), true);
@@ -87,8 +87,8 @@ public class DMGCalcTest {
         Yokimon y2 = new YokimonImpl("Squirtle", Attack.color.BLUE, map2, map2, YokimonImpl.DEFAULT_LEVEL,
                 YokimonImpl.DEFAULT_GROWRATE, Collections.emptyList(), true);
 
-        assertEquals(EXP_VAL_MULT1, toTest.getDMG(y1,y2,a1));
-        assertEquals(EXP_VAL_MULT2, toTest.getDMG(y1,y2,a2));
+        assertEquals(EXP_VAL_MULT1, toTest.getDMG(y1, y2, a1));
+        assertEquals(EXP_VAL_MULT2, toTest.getDMG(y1, y2, a2));
 
     }
 }
