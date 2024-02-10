9680c2867f0febba67071b1469c5d6741400f097
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-10 14:50:06 +0100

    Compliance of GameMapImpl to static code analyzers

--- src/main/java/io/github/yokigroup/world/GameMapImpl.java
+++ src/main/java/io/github/yokigroup/world/GameMapImpl.java
@@ -9,15 +9,23 @@ import io.github.yokigroup.world.tile.Tile;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
+/**
+ * The map of the game.
+ * Contains tiles, their entities and the player world position.
+ * Allows the player to switch screens (tiles) by walking around.
+ */
 public class GameMapImpl implements GameMap {
     private final WFCWrapper wfc;
     private final Map<Pair<Integer, Integer>, Tile> tileMap;
     private Pair<Integer, Integer> worldPlayerPosition;
 
+    /**
+     * Initializes the game map through the usage of the wave function collapse algorithm.
+     * @param dimensions The dimensions of the map in tiles.
+     */
     public GameMapImpl(final Pair<Integer, Integer> dimensions) {
-        // FIXME: add proper wfc initialization
+        // TODO: add proper wfc initialization
         this.wfc = new WFCWrapperImpl(dimensions, Collections.emptySet());
         this.tileMap = new HashMap<>();
         // TODO: initialize the map through the WFC algorithm
