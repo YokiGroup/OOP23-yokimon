75a91bc7a2dea5e82f28444f1a7e3f0ebfb02262
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-10 11:31:14 +0100

    Fix Hitbox collision detections.

--- src/main/java/io/github/yokigroup/world/entity/hitbox/HitboxImpl.java
+++ src/main/java/io/github/yokigroup/world/entity/hitbox/HitboxImpl.java
@@ -2,31 +2,39 @@ package io.github.yokigroup.world.entity.hitbox;
 
 import io.github.yokigroup.util.Vector2;
 import io.github.yokigroup.util.Vector2Impl;
-import org.dyn4j.geometry.Shape;
+import org.dyn4j.collision.Filter;
+import org.dyn4j.dynamics.Body;
+import org.dyn4j.dynamics.BodyFixture;
+import org.dyn4j.geometry.Transform;
+import org.dyn4j.world.DetectFilter;
+import org.dyn4j.world.World;
+import org.dyn4j.world.result.ConvexDetectResult;
+import org.dyn4j.world.result.DetectResult;
+
+import java.util.Iterator;
 
 /**
  * An abstract binding to the Shape class.
  */
 public abstract class HitboxImpl implements Hitbox {
-    private final Shape shape;
-    private Vector2 position;
+    private final Body body;
 
     /**
      * Creates a hitbox.
-     * @param shape The shape of the hitbox.
+     * @param body The body of the hitbox.
      * @param position The position of the hitbox.
      */
-    protected HitboxImpl(final Shape shape, final Vector2 position) {
-        this.shape = shape;
+    protected HitboxImpl(final Body body, final Vector2 position) {
+        this.body = body;
         this.setPosition(position);
     }
 
     /**
      * Creates hitbox at the origin.
-     * @param shape The shape of the hitbox.
+     * @param body The body of the hitbox.
      */
-    protected HitboxImpl(final Shape shape) {
-        this(shape, new Vector2Impl(0.0d, 0.0d));
+    protected HitboxImpl(final Body body) {
+        this(body, new Vector2Impl(0.0d, 0.0d));
     }
 
     /**
@@ -36,18 +44,40 @@ public abstract class HitboxImpl implements Hitbox {
      */
     @Override
     public boolean collidesWith(final Hitbox other) {
-        return this.shape.createAABB().overlaps(other.getShape().createAABB());
+        final World<Body> world = new World<>();
+        world.addBody(other.getBody());
+        DetectFilter<Body, BodyFixture> filter = new DetectFilter<>(true, true, null);
+        Iterator<ConvexDetectResult<Body, BodyFixture>> results = world.detectIterator(
+                this.body.getFixture(0).getShape(),
+                this.body.getTransform(),
+                filter
+        );
+        boolean collides = results.hasNext();
+        world.removeBody(other.getBody());
+        return collides;
     }
 
     @Override
     public final void setPosition(final Vector2 pos) {
-        this.position = pos;
-        this.shape.translate(0.0d, 0.0d);
-        this.shape.translate(pos.getX(), pos.getY());
+        final Transform transform = this.body.getTransform().copy();
+        transform.setTranslation(pos.getX(), pos.getY());
+        this.body.setTransform(transform);
     }
 
     @Override
     public final Vector2 getPosition() {
-        return this.position;
+        return new Vector2Impl(
+                this.body.getTransform().getTranslationX(),
+                this.body.getTransform().getTranslationY()
+        );
+    }
+
+    @Override
+    public final Body getBody() {
+        final Body bodyCopy = new Body();
+        this.body.getFixtures()
+                .forEach(f -> bodyCopy.addFixture(new BodyFixture(f.getShape())));
+        bodyCopy.translate(getPosition().getX(), getPosition().getY());
+        return bodyCopy;
     }
 }
