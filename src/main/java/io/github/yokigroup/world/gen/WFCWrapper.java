a7ef7001c8b9730a639fd66d30cd47ea103e2426
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-10 13:29:32 +0100

    Add base WFCWrapper implementation

--- src/main/java/io/github/yokigroup/world/gen/WFCWrapper.java
+++ src/main/java/io/github/yokigroup/world/gen/WFCWrapper.java
@@ -9,12 +9,6 @@ import java.util.Set;
  * A wrapper for a Wave Function Collapse library, can also be used for an implementation of it.
  */
 public interface WFCWrapper {
-    /**
-     * Initializes the wave function collapse algorithm with the tile shape's rules.
-     * @param dimensions The dimensions of the 2d map.
-     * @param shapes All the shapes that will be generated.
-     */
-    void initializeWFC(Pair<Integer, Integer> dimensions, Set<TileShape> shapes);
 
     /**
      * Sets one of the tiles in the map to be a specific one.
--- src/main/java/io/github/yokigroup/world/gen/WFCWrapperImpl.java
+++ src/main/java/io/github/yokigroup/world/gen/WFCWrapperImpl.java
@@ -1,4 +1,34 @@
 package io.github.yokigroup.world.gen;
 
-public class WFCWrapperImpl {
+import io.github.yokigroup.util.Pair;
+import io.github.yokigroup.world.tile.Tile;
+
+import java.util.Set;
+
+public class WFCWrapperImpl implements WFCWrapper {
+    private final WaveFunctionCollapse wfc = new WaveFunctionCollapseImpl();
+
+    /**
+     * Initializes the wave function collapse algorithm with the tile shape's rules.
+     * @param dimensions The dimensions of the 2d map.
+     * @param shapes All the shapes that will be generated.
+     */
+    public WFCWrapperImpl(final Pair<Integer, Integer> dimensions, final Set<TileShape> shapes) {
+
+    }
+
+    @Override
+    public void setStaticTile(final Pair<Integer, Integer> position, final Tile tile) {
+
+    }
+
+    @Override
+    public void runWFC() {
+
+    }
+
+    @Override
+    public Tile getTileAt(final Pair<Integer, Integer> position) {
+        return null;
+    }
 }
