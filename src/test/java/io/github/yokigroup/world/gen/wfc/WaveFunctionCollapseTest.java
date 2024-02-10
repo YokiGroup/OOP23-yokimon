328f651b52870e41a9ac9cd9258c58093a1f8984
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-10 16:03:37 +0100

    Fix empty WaveFunctionCollapse tests

--- src/test/java/io/github/yokigroup/world/gen/wfc/WaveFunctionCollapseTest.java
+++ src/test/java/io/github/yokigroup/world/gen/wfc/WaveFunctionCollapseTest.java
@@ -2,12 +2,13 @@ package io.github.yokigroup.world.gen.wfc;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
 class WaveFunctionCollapseTest {
 
     @Test
     void getShapeAt() {
+
     }
 
     @Test
