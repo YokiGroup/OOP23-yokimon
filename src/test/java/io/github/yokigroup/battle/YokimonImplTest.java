1649b4a5a51dc94bb2b2841ae605b50516145a08
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-10 16:24:06 +0100

    New Test YokimonImpl class

--- src/test/java/io/github/yokigroup/battle/YokimonImplTest.java
+++ src/test/java/io/github/yokigroup/battle/YokimonImplTest.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.*;
+import java.util.HashMap;
+import java.util.Map;
+
 class YokimonImplTest {
+
 
 
     @Test
@@ -16,6 +20,24 @@ class YokimonImplTest {
     }
     @Test
     void getAllStats() {
+        Attack shadowBall = new AttackImpl("Shadow ball", Color.BLACK, 90, Attack.effect.NONE);
+        Attack strongPunch = new AttackImpl("strong punch", Color.RED, 80, Attack.effect.NONE);
+        Attack curse = new AttackImpl("curse", Color.PURPLE, 70, Attack.effect.NONE);
+        Attack slap = new AttackImpl("slap", Color.WHITE, 40, Attack.effect.NONE);
+        Map<Integer, Attack> stone= new HashMap<>();
+        stone.put(1, slap);
+        stone.put(3, strongPunch);
+        stone.put(7, curse);
+        stone.put(15, shadowBall);
+        Map<Yokimon.Stats, Integer> baseStats= new HashMap<>();
+        baseStats.put(Yokimon.Stats.HP, 90);
+        baseStats.put(Yokimon.Stats.ATK, 80);
+        baseStats.put(Yokimon.Stats.DEF, 100);
+        baseStats.put(Yokimon.Stats.SPD, 40);
+
+        Yokimon oni = new YokimonImpl("oni", Color.RED, baseStats, Yokimon.GrowthRate.MEDIUM, 1, stone );
+        oni.takeXp(125);
+        assertEquals(5, oni.getLevel());
     }
 
     @Test
