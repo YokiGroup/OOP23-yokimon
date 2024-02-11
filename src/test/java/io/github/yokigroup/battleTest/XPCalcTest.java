af36ffdc040a4ba5b12448e33c87a375c223a1d5
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-11 13:57:40 +0100

    Complete implementation of test method in XPCalcTest (still need to fix bug to actually run tests)

--- src/test/java/io/github/yokigroup/battleTest/XPCalcTest.java
+++ src/test/java/io/github/yokigroup/battleTest/XPCalcTest.java
@@ -1,6 +1,10 @@
 package io.github.yokigroup.battleTest;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import io.github.yokigroup.battle.YokimonDatabase;
+import io.github.yokigroup.battle.xpcalculator.FullImplXPCalculator;
 import io.github.yokigroup.battle.xpcalculator.XPCalculator;
 import io.github.yokigroup.battle.Yokimon;
 import io.github.yokigroup.battle.YokimonImpl;
@@ -17,18 +21,19 @@ public class XPCalcTest {
 
     private static final int EXP_VAL_DUMMY1 = 0;
     private static final int EXP_VAL_DUMMY2 = 300;
-    private static final int EXP_VAL_DUMMY3 = 500;
+    private static final int EXP_VAL_DUMMY3 = 200;
+    private static final int EXP_VAL_FULL1 = 0;
+    private static final int EXP_VAL_FULL2 = 0;
+    private static final int EXP_VAL_FULL3 = 0;
 
     private static Yokimon y1, y2, y3;
 
-    //FIXME -> fix yokimon constructor or get actual complete yokimons as test variables
+    //FIXME -> still giving NullPointerException
     @BeforeEach
     public void init() {
-        /*
-        y1 = new YokimonImpl("Blue");
-        y2 = new YokimonImpl("Red");
-        y3 = new YokimonImpl("Yellow");
-         */
+        y1 = YokimonDatabase.getOni();
+        y2 = YokimonDatabase.getBaku();
+        y3 = YokimonDatabase.getNekomata();
     }
 
     /**
@@ -37,11 +42,22 @@ public class XPCalcTest {
     @Test public void testDummyImpl() {
         XPCalculator toTest = new DummyImplXPCalculator();
 
-        /*
         assertEquals(EXP_VAL_DUMMY1, toTest.getXP(List.of()));
         assertEquals(EXP_VAL_DUMMY2, toTest.getXP(List.of(y1, y2, y3)));
         assertNotEquals(EXP_VAL_DUMMY3, toTest.getXP(List.of(y1, y2)));
-         */
+
+    }
+
+    /**
+     * Testing full implementation.
+     */
+    //TODO MUST TEST THIS CLASS
+    @Test public void testFullImpl() {
+        XPCalculator toTest = new FullImplXPCalculator();
+
+        assertEquals(EXP_VAL_FULL1, toTest.getXP(List.of()));
+        assertEquals(EXP_VAL_FULL2, toTest.getXP(List.of(y1, y2, y3)));
+        assertNotEquals(EXP_VAL_FULL3, toTest.getXP(List.of(y1, y2)));
 
     }
 }
