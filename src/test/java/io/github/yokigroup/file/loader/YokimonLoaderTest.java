8db0e496d2c93d744d2ef347b4651ce7dd70e4be
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-12 00:07:05 +0100

    Add YokimonLoaderTest

---
+++ src/test/java/io/github/yokigroup/file/loader/YokimonLoaderTest.java
@@ -0,0 +1,24 @@
+package io.github.yokigroup.file.loader;
+
+import io.github.yokigroup.battle.Yokimon;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class YokimonLoaderTest {
+    private static YokimonLoader loader;
+
+    @BeforeEach
+    void setUp() {
+        loader = new YokimonLoader();
+    }
+
+    @Test
+    void load() {
+        Yokimon y = loader.load(1);
+
+        assertEquals("Pippo", y.getName());
+        assertEquals(Yokimon.GrowthRate.SLOW, y.getGrowRate());
+    }
+}
