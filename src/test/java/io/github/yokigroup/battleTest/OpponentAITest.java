846974a6e6bdfb106c1e025e8a98f6268af35b87
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-11 13:42:19 +0100

    Complete implementation of test method in OpponentAITest (still need to fix bug to actually run tests)

--- src/test/java/io/github/yokigroup/battleTest/OpponentAITest.java
+++ src/test/java/io/github/yokigroup/battleTest/OpponentAITest.java
@@ -7,72 +7,69 @@ import java.util.Optional;
 
 import io.github.yokigroup.battle.*;
 import io.github.yokigroup.battle.dmgcalculator.BasicImplDmgCalculator;
+import io.github.yokigroup.battle.dmgcalculator.FullImplDmgCalculator;
+import io.github.yokigroup.battle.opponentai.FullImplOpponentAI;
 import io.github.yokigroup.battle.opponentai.OpponentAI;
 import io.github.yokigroup.battle.opponentai.DummyImplOpponentAI;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 /**
  * Test class for Opponent AI.
  */
 public class OpponentAITest {
-
-    private static final int ATK_VALUE1 = 150;
-    private static final int DEF_VALUE1 = 80;
-    private static final int ATK_VALUE2 = 120;
-    private static final int DEF_VALUE2 = 90;
-    private static final int PWR_VALUE1 = 35;
-
-    private static Yokimon y1, y2, y3;
+    private static Yokimon y1, y2, y3, y4;
     private static Attack a1;
 
 
-    //FIXME -> fix yokimon constructor or get actual complete yokimons as test variables
+    //FIXME -> still giving NullPointerException
     @BeforeEach
     public void init() {
-
-        /*
-        a1 = new AttackImpl("Bubble", Color.BLACK, PWR_VALUE1, null);
-
-        Map<Yokimon.Stats, Integer> map1Stats = new HashMap<>();
-        map1Stats.put(Yokimon.Stats.ATK, ATK_VALUE1);
-        map1Stats.put(Yokimon.Stats.DEF, DEF_VALUE1);
-
-        Map<Yokimon.Stats, Integer> map2Stats = new HashMap<>();
-        map2Stats.put(Yokimon.Stats.ATK, ATK_VALUE2);
-        map2Stats.put(Yokimon.Stats.DEF, DEF_VALUE2);
-
-        Map<Integer, Attack> map1Learnables = new HashMap<>();
-        map1Learnables.put(YokimonImpl.DEFAULT_LEVEL, a1);
-
-        y1 = new YokimonImpl("Caterpie", Color.PURPLE, map1Stats, YokimonImpl.DEFAULT_GROWRATE,
-                YokimonImpl.DEFAULT_LEVEL, map1Learnables);
-
-        y2 = new YokimonImpl("Squirtle", Color.BLACK, map2Stats, YokimonImpl.DEFAULT_GROWRATE,
-                YokimonImpl.DEFAULT_LEVEL, map1Learnables);
-
-        y3 = new YokimonImpl("Meowtwo", Color.WHITE, map1Stats, YokimonImpl.DEFAULT_GROWRATE,
-                YokimonImpl.DEFAULT_LEVEL, map1Learnables);
-
-         */
+        a1 = YokimonDatabase.strongPunch;
+        y1 = null;                          //TODO: INSTANTIATE YOKIMON WITH NO MOVES AVAILABLE
+        y2 = null;                          //TODO: INSTANTIATE YOKIMON WITH ONE MOVE AVAILABLE (a1)
+        y3 = YokimonDatabase.getNekomata();
+        y4 = YokimonDatabase.getOni();
     }
 
     /**
      * Testing dummy implementation.
      */
     @Test public void testDummyImpl() {
-        /*
-        OpponentAI toTest = new DummyImplOpponentAI(new BasicImplDmgCalculator());
+        OpponentAI toTest = new DummyImplOpponentAI();
 
         //empty
-        assertEquals(Optional.empty(), toTest.getMove(y1));
+        assertEquals(Optional.empty(), toTest.getMove(y1,y2));
         //one attack
-        assertEquals(Optional.of(a1), toTest.getMove(y2));
+        assertEquals(Optional.of(a1), toTest.getMove(y2,y1));
         //multiple attacks
-        assertEquals(Optional.of(a1), toTest.getMove(y3));
+        assertEquals(Optional.of(a1), toTest.getMove(y3,y1));
+    }
 
-         */
+    //TODO MUST TEST THIS CLASS!!!!!!!!!!
+    @Test public void testFullImplBasicDMGCalc() {
+        OpponentAI toTest = new FullImplOpponentAI(new BasicImplDmgCalculator());
+
+        //empty
+        assertEquals(Optional.empty(), toTest.getMove(y1,y2));
+        //one attack
+        assertEquals(Optional.of(a1), toTest.getMove(y2,y1));
+        //multiple attacks. BASIC --> the Attack with the biggest POWER value
+        assertEquals(Optional.of(YokimonDatabase.shadowBall), toTest.getMove(y3,y4));
+    }
+
+    //TODO MUST TEST THIS CLASS!!!!!!!!!!
+    @Test public void testFullImplFullDMGCalc() {
+        OpponentAI toTest = new FullImplOpponentAI(new FullImplDmgCalculator());
+
+        //multiple attacks. FULL --> first, the Attack with the biggest POWER value
+        assertEquals(Optional.of(YokimonDatabase.curse), toTest.getMove(y3,y4));
+        //                           then, a random different one from the list
+        assertNotEquals(Optional.of(YokimonDatabase.curse), toTest.getMove(y3,y4));
+        //                           then again the most powerful attack
+        assertEquals(Optional.of(YokimonDatabase.curse), toTest.getMove(y3,y4));
     }
 }
