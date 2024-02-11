55211878d26876b5463caa3db8ce950276202eb1
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-11 13:41:29 +0100

    Corrections in NextYokTest

--- src/test/java/io/github/yokigroup/battleTest/NextYokTest.java
+++ src/test/java/io/github/yokigroup/battleTest/NextYokTest.java
@@ -13,52 +13,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Test class for Next Yokimon.
  */
 public class NextYokTest {
-
-    private static final int ATK_VALUE1 = 150;
-    private static final int DEF_VALUE1 = 80;
-    private static final int ATK_VALUE2 = 120;
-    private static final int DEF_VALUE2 = 90;
-    private static final int PWR_VALUE1 = 35;
-
     private static Yokimon y1, y2, y3;
-    private static Attack a1;
 
-    //FIXME -> fix yokimon constructor or get actual complete yokimons as test variables
+    //FIXME -> still giving NullPointerException
     @BeforeEach
     public void init() {
-        /*
-
-        a1 = new AttackImpl("Bubble", Color.BLACK, PWR_VALUE1, null);
-
-        Map<Yokimon.Stats, Integer> map1Stats = new HashMap<>();
-        map1Stats.put(Yokimon.Stats.ATK, ATK_VALUE1);
-        map1Stats.put(Yokimon.Stats.DEF, DEF_VALUE1);
-
-        Map<Yokimon.Stats, Integer> map2Stats = new HashMap<>();
-        map2Stats.put(Yokimon.Stats.ATK, ATK_VALUE2);
-        map2Stats.put(Yokimon.Stats.DEF, DEF_VALUE2);
-
-        Map<Integer, Attack> map1Learnables = new HashMap<>();
-        map1Learnables.put(YokimonImpl.DEFAULT_LEVEL, a1);
-
-
-        y1 = new YokimonImpl("Caterpie", Color.PURPLE, map1Stats, YokimonImpl.DEFAULT_GROWRATE,
-                YokimonImpl.DEFAULT_LEVEL, map1Learnables);
-
-        y2 = new YokimonImpl("Squirtle", Color.BLACK, map2Stats, YokimonImpl.DEFAULT_GROWRATE,
-                YokimonImpl.DEFAULT_LEVEL, map1Learnables);
-
-        y3 = new YokimonImpl("Meowtwo", Color.WHITE, map1Stats, YokimonImpl.DEFAULT_GROWRATE,
-                YokimonImpl.DEFAULT_LEVEL, map1Learnables);
-
-         */
+        y1 = YokimonDatabase.getOni();
+        y2 = YokimonDatabase.getBaku();
+        y3 = YokimonDatabase.getKitsune();
     }
 
     /**
      * Testing dummy implementation.
      */
     @Test public void testDummyImpl() {
-        /*
         NextYokimon toTest = new DummyImplNextYokimon();
 
         List<Yokimon> party1 = new LinkedList<>();
@@ -68,6 +36,11 @@ public class NextYokTest {
 
         party1.remove(y1);
         assertEquals(Optional.of(y2), toTest.getNext(party1));
-         */
+
+        party1.remove(y2);
+        assertEquals(Optional.of(y3), toTest.getNext(party1));
+
+        party1.remove(y3);
+        assertEquals(Optional.empty(), toTest.getNext(party1));
     }
 }
