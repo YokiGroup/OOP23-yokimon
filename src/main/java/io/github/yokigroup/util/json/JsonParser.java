0549e9a5ae678e73ae1080508cd894b0aee54a83
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-11 12:05:06 +0100

    Add javadoc to JsonParser

--- src/main/java/io/github/yokigroup/util/json/JsonParser.java
+++ src/main/java/io/github/yokigroup/util/json/JsonParser.java
@@ -5,6 +5,15 @@ package io.github.yokigroup.util.json;
  * @see <a href="https://datatracker.ietf.org/wg/jsonpath/about/">jsonpath</a>
  */
 public interface JsonParser {
+    /**
+     * Root dir of json files contained in the ClassPath.
+     */
     String ROOT = "io/github/yokigroup/utils/json";
+
+    /**
+     * @param jsonPath json path expression to evaluate
+     * @return value returned by parse cast to type {@param T}
+     * @param <T>
+     */
     <T> T read(String jsonPath);
 }
