8b4c3092cc7f0089dcb17cbf35aedc84c496d70b
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-12 21:49:09 +0100

    Rewrite more coherent tests. Substitute YokimonDatabase with YokimonLoader

--- src/test/java/io/github/yokigroup/battleTest/OpponentAITest.java
+++ src/test/java/io/github/yokigroup/battleTest/OpponentAITest.java
@@ -1,72 +1,61 @@
 package io.github.yokigroup.battleTest;
 
-import io.github.yokigroup.battle.*;
+import io.github.yokigroup.battle.Yokimon;
+import io.github.yokigroup.battle.dmgcalculator.BasicImplDmgCalculator;
+import io.github.yokigroup.battle.opponentai.*;
+import io.github.yokigroup.file.loader.AttackLoader;
+import io.github.yokigroup.file.loader.YokimonLoader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 /**
  * Test class for Opponent AI.
  */
 public class OpponentAITest {
-    private static Yokimon y1, y2, y3, y4;
-    private static Attack a1;
+    private static Yokimon y1, y2;
+    private final YokimonLoader yokimonLoader = new YokimonLoader();
+    private final AttackLoader attackLoader = new AttackLoader();
 
-
-    //FIXME -> still giving NullPointerException
     @BeforeEach
     public void init() {
-        a1 = YokimonDatabase.strongPunch;
-        y1 = null;                          //TODO: INSTANTIATE YOKIMON WITH NO MOVES AVAILABLE
-        y2 = null;                          //TODO: INSTANTIATE YOKIMON WITH ONE MOVE AVAILABLE (a1)
-        y3 = YokimonDatabase.getNekomata();
-        y4 = YokimonDatabase.getOni();
+        y1 = yokimonLoader.load(1);
+        y2 = yokimonLoader.load(2);
     }
 
     /**
      * Testing dummy implementation.
      */
     @Test public void testDummyImpl() {
-        /*
         OpponentAI toTest = new DummyImplOpponentAI();
 
-        //empty
-        assertEquals(Optional.empty(), toTest.getMove(y1,y2));
-        //one attack
-        assertEquals(Optional.of(a1), toTest.getMove(y2,y1));
-        //multiple attacks
-        assertEquals(Optional.of(a1), toTest.getMove(y3,y1));
+        assertEquals(Optional.of(attackLoader.load(4)).get().getName().toLowerCase(),
+                        toTest.getMove(y2, y1).get().getName().toLowerCase());
+        assertEquals(Optional.of(attackLoader.load(4)).get().getName().toLowerCase(),
+                toTest.getMove(y2, y1).get().getName().toLowerCase());
 
-         */
     }
 
-    //TODO MUST TEST THIS CLASS!!!!!!!!!!
+    //FIXME --> ON THE SECOND RUN IT DOESN'T CHANGE THE ATTACK, IT ALWAYS CHOOSES THE BEST ONE
+    //NOTE: IN ORDER TO TEST YOU NEED TO MODIFY THE JSON FILE SO THEY HAVE MORE ATTACKS AVAILABLE
     @Test public void testFullImplBasicDMGCalc() {
-        /*
         OpponentAI toTest = new FullImplOpponentAI(new BasicImplDmgCalculator());
 
-        //empty
-        assertEquals(Optional.empty(), toTest.getMove(y1,y2));
-        //one attack
-        assertEquals(Optional.of(a1), toTest.getMove(y2,y1));
         //multiple attacks. BASIC --> the Attack with the biggest POWER value
-        assertEquals(Optional.of(YokimonDatabase.shadowBall), toTest.getMove(y3,y4));
-
-         */
+        //                            first, the Attack with the biggest POWER value
+        assertEquals(Optional.of(attackLoader.load(1)).get().getName().toLowerCase(),
+                toTest.getMove(y2, y1).get().getName().toLowerCase());
+        //                              then, another -random- one
+        assertNotEquals(Optional.of(attackLoader.load(1)).get().getName().toLowerCase(),
+                toTest.getMove(y2, y1).get().getName().toLowerCase());
+        //                              then again the best one
+        assertEquals(Optional.of(attackLoader.load(1)).get().getName().toLowerCase(),
+                toTest.getMove(y2, y1).get().getName().toLowerCase());
     }
 
-    //TODO MUST TEST THIS CLASS!!!!!!!!!!
-    @Test public void testFullImplFullDMGCalc() {
-        /*
-        OpponentAI toTest = new FullImplOpponentAI(new FullImplDmgCalculator());
-
-        //multiple attacks. FULL --> first, the Attack with the biggest POWER value
-        assertEquals(Optional.of(YokimonDatabase.curse), toTest.getMove(y3,y4));
-        //                           then, a random different one from the list
-        assertNotEquals(Optional.of(YokimonDatabase.curse), toTest.getMove(y3,y4));
-        //                           then again the most powerful attack
-        assertEquals(Optional.of(YokimonDatabase.curse), toTest.getMove(y3,y4));
-
-         */
-    }
 }
