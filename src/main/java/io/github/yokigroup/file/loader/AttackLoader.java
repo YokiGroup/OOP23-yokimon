910567b539ef2a7e6adc5db86df75d72b3345b3c
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-12 13:09:38 +0100

    Fix wrong constant value in AttackLoader

--- src/main/java/io/github/yokigroup/file/loader/AttackLoader.java
+++ src/main/java/io/github/yokigroup/file/loader/AttackLoader.java
@@ -6,7 +6,7 @@ import io.github.yokigroup.battle.Color;
 import io.github.yokigroup.util.json.JsonParser;
 
 public class AttackLoader extends AbstractJsonLoader<Attack> {
-    private static final String ATTACKJSONRPATH = "yokimons.json";
+    private static final String ATTACKJSONRPATH = "skills.json";
     private static final String ATTACK_NAME_JPATHF = "$.%d.name";
     private static final String ATTACK_COLOR_JPATHF = "$.%d.color";
     private static final String ATTACK_POWER_JPATHF = "$.%d.power";
@@ -20,9 +20,9 @@ public class AttackLoader extends AbstractJsonLoader<Attack> {
     public Attack load(int id) {
         JsonParser parser = getParser();
 
+        int power = parser.read(String.format(ATTACK_POWER_JPATHF, id));
         String name = parser.read(String.format(ATTACK_NAME_JPATHF, id));
         Color color = Color.valueOf(parser.read(String.format(ATTACK_COLOR_JPATHF, id)));
-        int power = parser.read(String.format(ATTACK_POWER_JPATHF, id));
         Attack.effect effect = Attack.effect.valueOf(parser.read(String.format(ATTACK_EFFECT_JPATHF, id)));
 
         return new AttackImpl(name, color, power, effect);
