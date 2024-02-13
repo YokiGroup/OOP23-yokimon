65362f3c1686f80eff418e721bfa6d0e7332488f
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-13 16:41:28 +0100

    Delete FightObserver.java

--- src/main/java/io/github/yokigroup/battle/fight/FightObserver.java
+++
@@ -1,8 +0,0 @@
-package io.github.yokigroup.battle.fight;
-
-/**
- * A class must implement this interface in order to subscribe to a fight.
- */
-public interface FightObserver {
-    void update (Fight fight, Fight.State state);
-}
