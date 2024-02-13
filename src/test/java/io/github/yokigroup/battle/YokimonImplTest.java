8510f81ea71f6b21bbf911baf032a8a6c009615f
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-13 18:20:23 +0100

    Update test

--- src/test/java/io/github/yokigroup/battle/YokimonImplTest.java
+++ src/test/java/io/github/yokigroup/battle/YokimonImplTest.java
@@ -1,5 +1,6 @@
 package io.github.yokigroup.battle;
 
+import io.github.yokigroup.battle.dmgcalculator.BasicImplDmgCalculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -117,16 +118,23 @@ class YokimonImplTest {
         stone.put(3, strongPunch);
         stone.put(7, curse);
         stone.put(15, shadowBall);
-        Map<Yokimon.Stats, Integer> baseStats= new HashMap<>();
-        baseStats.put(Yokimon.Stats.HP, 90);
-        baseStats.put(Yokimon.Stats.ATK, 80);
-        baseStats.put(Yokimon.Stats.DEF, 100);
-        baseStats.put(Yokimon.Stats.SPD, 40);
+        Map<Yokimon.Stats, Integer> baseStats1= new HashMap<>();
+        baseStats1.put(Yokimon.Stats.HP, 90);
+        baseStats1.put(Yokimon.Stats.ATK, 80);
+        baseStats1.put(Yokimon.Stats.DEF, 100);
+        baseStats1.put(Yokimon.Stats.SPD, 40);
+        Map<Yokimon.Stats, Integer> baseStats2= new HashMap<>();
+        baseStats2.put(Yokimon.Stats.HP, 120);
+        baseStats2.put(Yokimon.Stats.ATK, 100);
+        baseStats2.put(Yokimon.Stats.DEF, 60);
+        baseStats2.put(Yokimon.Stats.SPD, 70);
+        BasicImplDmgCalculator calc = new BasicImplDmgCalculator();
+        Yokimon oni = new YokimonImpl("oni", Color.RED, baseStats1, Yokimon.GrowthRate.MEDIUM, 10, stone );
+        Yokimon wani = new YokimonImpl("wani", Color.RED, baseStats2, Yokimon.GrowthRate.SLOW, 10, stone );
+        assertEquals(42, oni.getStat(Yokimon.Stats.HP));
 
-        Yokimon oni = new YokimonImpl("oni", Color.RED, baseStats, Yokimon.GrowthRate.MEDIUM, 1, stone );
+        assertEquals(40, calc.getDMG(oni, wani, slap));
 
 
-        oni.takeXp(216);
-        assertEquals(6, oni.getLevel());
     }
 }
