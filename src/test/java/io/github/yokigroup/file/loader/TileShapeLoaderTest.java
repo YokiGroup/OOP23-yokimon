b16fdb21ed3ae8de821184f1c0e642ad7e5233b3
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-13 21:59:00 +0100

    Create TileShapeLoaderTest

---
+++ src/test/java/io/github/yokigroup/file/loader/TileShapeLoaderTest.java
@@ -0,0 +1,17 @@
+package io.github.yokigroup.file.loader;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TileShapeLoaderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
