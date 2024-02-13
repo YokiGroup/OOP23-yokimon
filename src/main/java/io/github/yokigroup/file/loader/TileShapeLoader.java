62b24be98dc3771f74dc0506bf8e77bcad1f13e2
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-13 18:17:28 +0100

    Implement TileShapeLoader#getAll

--- src/main/java/io/github/yokigroup/file/loader/TileShapeLoader.java
+++ src/main/java/io/github/yokigroup/file/loader/TileShapeLoader.java
@@ -1,17 +1,19 @@
 package io.github.yokigroup.file.loader;
 
+import com.google.common.collect.ImmutableList;
 import io.github.yokigroup.util.Pair;
 import io.github.yokigroup.util.json.JsonParser;
 import io.github.yokigroup.util.json.PathNotFoundException;
 import io.github.yokigroup.world.gen.TileShape;
+import io.github.yokigroup.world.gen.TileShapeImpl;
 import io.github.yokigroup.world.tile.Tile;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class TileShapeLoader extends JsonLoader<TileShape>{
     private static final String TILEJSONRPATH = "tiles.json";
     //private static final String
+    private Map<Set<TileShape.TileDirections>, Set<Tile>> tiles = new HashMap<>();
 
     public TileShapeLoader() {
         super(TILEJSONRPATH);
@@ -26,17 +28,32 @@ public class TileShapeLoader extends JsonLoader<TileShape>{
         return null;
     }
 
-    public Set<TileShape> getAll(){
+    private void insertTile(Set<TileShape.TileDirections> dirs, Tile tile) {
+        Objects.requireNonNull(dirs);
+        Objects.requireNonNull(tile);
+
+        if(!tiles.containsKey(dirs)) {
+            tiles.put(dirs, new HashSet<>());
+        }
+        tiles.get(dirs).add(tile);
+    }
+
+    public Set<TileShape> getAll() {
         Set<TileShape> shapes = new HashSet<>();
         int i = 1;
 
-        try{
-            while(true){
+        if(tiles.isEmpty()){
+            try {
+                while(true) {
+                    var nextTile = load(i);
+                    insertTile(nextTile.x(), nextTile.y());
+                    i++;
+                }
+            }catch(PathNotFoundException ignored){ }
+        }
 
-                i++;
-            }
-        }catch(PathNotFoundException e){
-
+        for(var tileEntry: tiles.entrySet()) {
+            shapes.add(new TileShapeImpl(tileEntry.getValue(), tileEntry.getKey()));
         }
 
         return shapes;
