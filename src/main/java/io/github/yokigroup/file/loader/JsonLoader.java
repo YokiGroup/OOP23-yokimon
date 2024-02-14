ad39a56de6413898eeb46242b59cf9ba5fae92f3
= <giovanni.paone@studio.unibo.it>
2024-02-14 21:36:12 +0100

    Compliance of JsonLoader to static code analyzers

--- src/main/java/io/github/yokigroup/file/loader/JsonLoader.java
+++ src/main/java/io/github/yokigroup/file/loader/JsonLoader.java
@@ -12,28 +12,39 @@ import java.util.function.BiConsumer;
 public abstract class JsonLoader<T> {
     private final JsonParser parser;
 
-    protected final <T> T doUntilPathException(T aggregator, BiConsumer<T, Integer> fun) {
+    /**
+     * Performs BiConsumer {@param fun} until it encounters a {@link PathNotFoundException}. Useful in parser calls.
+     * @param aggregator aggregator passed in every
+     * @param fun function to invoke: First Parameter is the {@param aggregator} (list, set, map...), \
+     *            second parameter is an index.
+     * @return aggregator after {@param fun} has been run
+     * @param <T> aggregator type
+     */
+    protected final <T> T doUntilPathException(final T aggregator, final BiConsumer<T, Integer> fun) {
         int i = 0;
 
         try {
-            while(true) {
+            while (true) {
                 fun.accept(aggregator, i);
                 i++;
             }
-        }catch(PathNotFoundException ignored) { }
+        } catch (PathNotFoundException ignored) { }
 
         return aggregator;
     }
 
-    protected JsonParser getParser(){
+    /**
+     * @return the instanced parser
+     */
+    protected final JsonParser getParser() {
         return parser;
     }
 
     /**
      * @param relativePath path of json file relative to {@code JsonParser.ROOT}
      */
-    public JsonLoader(final String relativePath){
-        this.parser = new JsonParserImpl(JsonParser.ROOT+"/"+relativePath);
+    public JsonLoader(final String relativePath) {
+        this.parser = new JsonParserImpl(JsonParser.ROOT + "/" + relativePath);
     }
 
 }
