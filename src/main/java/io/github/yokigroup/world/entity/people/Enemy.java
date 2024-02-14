7b6e48e2cdf944e22f84dcc9902a93158cc2bbd2
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-14 10:12:00 +0100

    Change update

--- src/main/java/io/github/yokigroup/world/entity/people/Enemy.java
+++ src/main/java/io/github/yokigroup/world/entity/people/Enemy.java
@@ -2,12 +2,16 @@ package io.github.yokigroup.world.entity.people;
 
 import io.github.yokigroup.battle.Yokimon;
 import io.github.yokigroup.event.MessageHandler;
+import io.github.yokigroup.event.submodule.PartySubmodule;
+import io.github.yokigroup.event.submodule.PlayerCharacterSubmodule;
 import io.github.yokigroup.event.submodule.Submodule;
 import io.github.yokigroup.event.submodule.SubmoduleMapImpl;
+import io.github.yokigroup.world.entity.Altar;
 import io.github.yokigroup.world.entity.Position;
 import io.github.yokigroup.world.entity.hitbox.Hitbox;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Enemy class represents an enemy entity in the game world.
@@ -20,6 +24,7 @@ public class Enemy extends People {
     private static final double RADIUS_PLAYER = 6.00;
     /**
      * This value represent the maximum distance at which the enemy will
+     * go from his initial pos
      */
     private static final double RADIUS_INITIAL_POS = 5.00;
     private Enemy.state state;
@@ -59,7 +64,11 @@ public class Enemy extends People {
      */
     @Override
     public void update() {
-        
+        this.getMessageHandler().handle(PlayerCharacterSubmodule.class, pos -> {
+            Objects.requireNonNull(pos.getPosition().getPosition(), "Position of the player invalid");
+
+
+        });
 
     }
 }
