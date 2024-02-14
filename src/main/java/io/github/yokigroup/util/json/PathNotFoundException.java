3a6111459e4f838ba8eef00194f33f0721d70be8
= <giovanni.paone@studio.unibo.it>
2024-02-14 21:44:30 +0100

    Compliance of PathNotFoundException to static code analyzers

--- src/main/java/io/github/yokigroup/util/json/PathNotFoundException.java
+++ src/main/java/io/github/yokigroup/util/json/PathNotFoundException.java
@@ -1,19 +1,19 @@
 package io.github.yokigroup.util.json;
 
+/**
+ * Thrown when the path queried to the json parser is not found.
+ */
 public class PathNotFoundException extends RuntimeException {
-    public PathNotFoundException() {
-        super();
-    }
 
-    public PathNotFoundException(Throwable cause) {
+    /**
+     * Constructs a new runtime exception with the specified cause.
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A {@code null} value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
+    public PathNotFoundException(final Throwable cause) {
         super(cause);
     }
 
-    public PathNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public PathNotFoundException(String message) {
-        super(message);
-    }
 }
