76d65374c24a1a70d1a0d5a0e6bf60796b851f0e
= <giovanni.paone@studio.unibo.it>
2024-02-14 22:22:24 +0100

    Add handle method in MessageHandler with return value

--- src/main/java/io/github/yokigroup/event/MessageHandler.java
+++ src/main/java/io/github/yokigroup/event/MessageHandler.java
@@ -3,6 +3,7 @@ package io.github.yokigroup.event;
 import io.github.yokigroup.event.submodule.Submodule;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Responsible for the communication between entities and the submodules they interact with.
@@ -16,4 +17,14 @@ public interface MessageHandler {
      * @param <T> Submodule to handle
      */
     <T extends Submodule> void handle(Class<T> subModuleType, Consumer<T> handler);
+
+    /**
+     * handles an operation with a submodule, usually called by entities.
+     * Returns the return value of the handler function.
+     * @param subModuleType type of the submodule called in play
+     * @param handler handler function to determine what to do with the submodule
+     * @param <T> Submodule to handle
+     * @return return value of handler function
+     */
+    <T extends Submodule, E> E handle(Class<T> subModuleType, Function<T, E> handler);
 }
