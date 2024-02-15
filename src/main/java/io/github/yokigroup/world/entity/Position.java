0ca2cb6dcb7259f34bcb3d4694e87d893308c75d
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-15 10:48:23 +0100

    Stile changes

--- src/main/java/io/github/yokigroup/world/entity/Position.java
+++ src/main/java/io/github/yokigroup/world/entity/Position.java
@@ -4,7 +4,7 @@ import io.github.yokigroup.event.MessageHandler;
 import io.github.yokigroup.util.Vector2;
 
 /**
- * Interface of an Object which represent a Position, used Entity
+ * Interface of an Object which represent a Position, used Entity.
  */
 public interface Position {
 
@@ -12,41 +12,42 @@ public interface Position {
      * Return the position.
      * @return Vector
      */
-    public Vector2 getPosition();
+    Vector2 getPosition();
 
     /**
      * Set a new position, if it's not valid... .
      * @param pos new position
      */
-    public void setPosition(Vector2 pos);
+    void setPosition(Vector2 pos);
 
     /**
      * add a vector to the position.
      * @param vector vector to add
      */
-    public void movePosition(Vector2 vector);
+    void movePosition(Vector2 vector);
 
     /**
      * add a vector to the position and return the new value of pos without changing it.
      * @param vector vector to add
      * @return Position new
      */
-    public Position testTovePosition(Vector2 vector);
+    Position testMovePosition(Vector2 vector);
     /**
      * Return true if the position is valid in the map.
      * False if is not.
+     * @param messageHandler handler for messages
      * @return boolean
      */
-    public boolean isValid(MessageHandler messageHandler);
+    boolean isValid(MessageHandler messageHandler);
 
     /**
      * Return true if the distance from the pair passed is less then Radius
-     * Return false if radius is greater;
+     * Return false if radius is greater.
      * @param otherPos the other pair to check
      * @param radius min distance
      * @return boolean
      */
-    public boolean inRadius(Position otherPos, double radius);
+    boolean inRadius(Position otherPos, double radius);
 
 
 }
