4f4c3f9defe25d363dff1cf40e7dd0ec7e93c333
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-15 18:56:56 +0100

    Add TileBuilder class

--- src/main/java/io/github/yokigroup/world/tile/TileBuilder.java
+++ src/main/java/io/github/yokigroup/world/tile/TileBuilder.java
@@ -1,5 +1,42 @@
 package io.github.yokigroup.world.tile;
 
-public interface TileBuilder {
+import io.github.yokigroup.world.entity.Entity;
+import io.github.yokigroup.world.entity.hitbox.Hitbox;
 
+import java.util.Set;
+
+/**
+ * Interface for a tile builder class.
+ */
+public interface TileBuilder {
+    /**
+     * Adds a hitbox to the tile.
+     * @param hitbox The hitbox to add.
+     */
+    void addHitbox(Hitbox hitbox);
+
+    /**
+     * Adds multiple hitboxes to the tile.
+     * @param hitboxes The hitboxes to add.
+     */
+    void addAllHitboxes(Set<Hitbox> hitboxes);
+
+    /**
+     * Adds an entity to the tile.
+     * @param entity The entity to add.
+     */
+    void addEntity(Entity entity);
+
+    /**
+     * Adds multiple entities to the tile.
+     * @param entities The entities to add.
+     */
+    void addAllEntities(Set<Entity> entities);
+
+    /**
+     * Finalizes the tile object.
+     * @param id The id of the tile.
+     * @return The built tile.
+     */
+    Tile build(final int id);
 }
--- src/main/java/io/github/yokigroup/world/tile/TileBuilderImpl.java
+++ src/main/java/io/github/yokigroup/world/tile/TileBuilderImpl.java
@@ -1,5 +1,45 @@
 package io.github.yokigroup.world.tile;
 
-public class TileBuilderImpl implements TileBuilder {
+import io.github.yokigroup.world.entity.Entity;
+import io.github.yokigroup.world.entity.hitbox.Hitbox;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Implementation of a builder class to create a tile object.
+ */
+public class TileBuilderImpl implements TileBuilder {
+    final Set<Entity> entities;
+    final Set<Hitbox> hitboxes;
+
+    public TileBuilderImpl() {
+        this.entities = new HashSet<>();
+        this.hitboxes = new HashSet<>();
+    }
+
+    @Override
+    public final void addHitbox(final Hitbox hitbox) {
+        this.hitboxes.add(hitbox);
+    }
+
+    @Override
+    public final void addAllHitboxes(final Set<Hitbox> hitboxes) {
+        this.hitboxes.addAll(hitboxes);
+    }
+
+    @Override
+    public final void addEntity(final Entity entity) {
+        this.entities.add(entity);
+    }
+
+    @Override
+    public final void addAllEntities(final Set<Entity> entities) {
+        this.entities.addAll(entities);
+    }
+
+    @Override
+    public Tile build(final int id) {
+        return new TileImpl(id, this.hitboxes, this.entities);
+    }
 }
