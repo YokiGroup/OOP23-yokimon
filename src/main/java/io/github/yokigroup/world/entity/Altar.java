d7ae17c700b54e01f66185ac4d3d0a3dc50e3900
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-15 10:50:00 +0100

    Stile changes + add id

--- src/main/java/io/github/yokigroup/world/entity/Altar.java
+++ src/main/java/io/github/yokigroup/world/entity/Altar.java
@@ -6,44 +6,45 @@ import io.github.yokigroup.event.MessageHandler;
 import io.github.yokigroup.event.submodule.PartySubmodule;
 import io.github.yokigroup.event.submodule.PlayerCharacterSubmodule;
 import io.github.yokigroup.world.entity.hitbox.Hitbox;
-import io.github.yokigroup.world.entity.people.Player;
 
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * Altar class represents an altar entity in the game world.
  * An altar gives a Yokimon to the player at the first interaction.
  */
 public class Altar extends Entity {
-
     private final Yokimon gift;
-    private altarState state;
-    private final static double RADIUS = 4;
+    private AltarState state;
+    private static final double RADIUS = 4;
+
     /**
      * Constructs an Altar object with the specified attributes.
-     * @param Pos The position of the Altar
-     * @param Hitbox The hitbox of the Altar
+     * @param id id of the altar
+     * @param pos The position of the Altar
+     * @param hitBox The hitBox of the Altar
      * @param yokimon The Yokimon to give at the first interaction
+     * @param messageHandler Message handler used to handle events
      */
-    public Altar(Position Pos, Hitbox Hitbox, Yokimon yokimon, MessageHandler messageHandler) {
-        super(Pos, Hitbox, messageHandler);
+    public Altar(final int id, final Position pos, final Hitbox hitBox,
+                 final Yokimon yokimon, final MessageHandler messageHandler) {
+        super(id, pos, hitBox, messageHandler);
         this.gift = new YokimonImpl(yokimon);
-        this.state = altarState.powered;
+        this.state = AltarState.POWERED;
     }
 
     /**
      * Returns the current state of the Altar (if it's used or not).
      * @return altarState Current state of the Altar
      */
-    public Altar.altarState getState() {
+    public final Altar.AltarState getState() {
         return this.state;
     }
     /**
      * Returns a Yokimon if the altar is powered, otherwise an optional empty.
      * @return Optional<Yokimon> The Yokimon from the Altar
      */
-    public Yokimon getNewYokimon() {
+    public final Yokimon getNewYokimon() {
             return new YokimonImpl(this.gift);
     }
 
@@ -54,11 +55,15 @@ public class Altar extends Entity {
     public void update() {
         this.getMessageHandler().handle(PlayerCharacterSubmodule.class, pos -> {
             Objects.requireNonNull(pos.getPosition().getPosition(), "Position of the player invalid");
-            if(pos.getPosition().isValid(this.getMessageHandler()) && pos.getPosition().inRadius(this.getPos(), RADIUS)
-                    && this.state == altarState.powered){
-                this.getMessageHandler().handle(PartySubmodule.class, party ->{
+
+            if (pos.getPosition().isValid(this.getMessageHandler())
+                    && pos.getPosition().inRadius(this.getPos(), RADIUS)
+                    && this.state == AltarState.POWERED) {
+
+                    this.getMessageHandler().handle(PartySubmodule.class, party -> {
                     party.addYokimon(this.getNewYokimon());
-                    this.state = altarState.used;
+                    this.state = AltarState.USED;
+
                 });
 
             }
@@ -68,9 +73,15 @@ public class Altar extends Entity {
     /**
      * Represents the state of the Altar (used or powered).
      */
-    public enum altarState {
-        used,
-        powered
+    public enum AltarState {
+        /**
+         * If the altar is in this state will no longer give a yokimon
+         */
+        USED,
+        /**
+         * If the altar is in this state is active and will give a yokimon
+         */
+        POWERED
     }
 
 }
