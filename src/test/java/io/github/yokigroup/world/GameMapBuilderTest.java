7c56106d21d35495d8218993602adb8667a199b9
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-15 20:09:47 +0100

    Add GameMapBuilder empty tests

---
+++ src/test/java/io/github/yokigroup/world/GameMapBuilderTest.java
@@ -0,0 +1,28 @@
+package io.github.yokigroup.world;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameMapBuilderTest {
+
+    @Test
+    void setMapDimensions() {
+        // TODO: implement test
+    }
+
+    @Test
+    void setPlayerTileMapPosition() {
+        // TODO: implement test
+    }
+
+    @Test
+    void setStaticTileAt() {
+        // TODO: implement test
+    }
+
+    @Test
+    void build() {
+        // TODO: implement test
+    }
+}
