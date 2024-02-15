aa6cb18eb26c95a7357ae3c82f4cafe5980a5840
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-15 13:34:37 +0100

    Fix YokimonLoaderTest

--- src/test/java/io/github/yokigroup/file/loader/YokimonLoaderTest.java
+++ src/test/java/io/github/yokigroup/file/loader/YokimonLoaderTest.java
@@ -1,5 +1,6 @@
 package io.github.yokigroup.file.loader;
 
+import io.github.yokigroup.battle.Color;
 import io.github.yokigroup.battle.Yokimon;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,9 +22,10 @@ class YokimonLoaderTest {
         AttackLoader attackLoader = new AttackLoader();
         Yokimon y = loader.load(1);
 
-        assertEquals("Pippo", y.getName());
-        assertEquals(Yokimon.GrowthRate.SLOW, y.getGrowRate());
-        assertEquals(attackLoader.load(1).getName(), y.getAttacks().get(0).getName());
+        assertEquals("Pippo Pugile", y.getName());
+        assertEquals(Yokimon.GrowthRate.FAST, y.getGrowRate());
+        assertEquals(Color.BLACK, y.getYokimonColor());
+        assertEquals(attackLoader.load(1), y.getAttacks().get(0));
         // FIXME check the validity of all yokimon fields (with reflection if need be)
 
     }
