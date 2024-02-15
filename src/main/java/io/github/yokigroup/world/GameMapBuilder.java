45a77929dc05095245e89cef12260bd2a2954e3f
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-15 20:08:47 +0100

    Add GameMapBuilder implementation

--- src/main/java/io/github/yokigroup/world/GameMapBuilder.java
+++ src/main/java/io/github/yokigroup/world/GameMapBuilder.java
@@ -1,5 +1,32 @@
 package io.github.yokigroup.world;
 
+import io.github.yokigroup.util.Pair;
+import io.github.yokigroup.world.tile.Tile;
+
 public interface GameMapBuilder {
-    //TODO: dummy
+
+    /**
+     * Sets the dimensions of the map.
+     * @param dimensions The width and height dimensions of the map.
+     */
+    void setMapDimensions(Pair<Integer, Integer> dimensions);
+
+    /**
+     * Sets the starting position of the player on the map.
+     * @param position The position of the player.
+     */
+    void setPlayerTileMapPosition(Pair<Integer, Integer> position);
+
+    /**
+     * Sets a static tile in the map (won't be overridden by the generation).
+     * @param position The position to set the tile at.
+     * @param tile The tile to set it to.
+     */
+    void setStaticTileAt(Pair<Integer, Integer> position, Tile tile);
+
+    /**
+     * Finalizes the GameMap object.
+     * @return The built GameMap.
+     */
+    GameMap build();
 }
--- src/main/java/io/github/yokigroup/world/GameMapBuilderImpl.java
+++ src/main/java/io/github/yokigroup/world/GameMapBuilderImpl.java
@@ -1,5 +1,58 @@
 package io.github.yokigroup.world;
 
+import io.github.yokigroup.file.loader.TileShapeLoader;
+import io.github.yokigroup.util.Pair;
+import io.github.yokigroup.world.gen.WFCWrapper;
+import io.github.yokigroup.world.gen.WFCWrapperImpl;
+import io.github.yokigroup.world.tile.Tile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Implementation of a builder class to create a GameMap object.
+ */
 public class GameMapBuilderImpl implements GameMapBuilder {
-    //TODO: dummy
+    private final Map<Pair<Integer, Integer>, Tile> tileMap;
+    private Pair<Integer, Integer> playerTileMapPosition;
+    private Pair<Integer, Integer> mapDimensions;
+
+    /**
+     * Creates a 1x1 map with the player starting at 0, 0.
+     */
+    public GameMapBuilderImpl() {
+        this.tileMap = new HashMap<>();
+        this.playerTileMapPosition = new Pair<>(0, 0);
+        this.mapDimensions = new Pair<>(1, 1);
+    }
+
+    @Override
+    public final void setMapDimensions(final Pair<Integer, Integer> dimensions) {
+        this.mapDimensions = dimensions;
+    }
+
+    @Override
+    public final void setPlayerTileMapPosition(final Pair<Integer, Integer> position) {
+        this.playerTileMapPosition = position;
+    }
+
+    @Override
+    public final void setStaticTileAt(final Pair<Integer, Integer> position, final Tile tile) {
+        this.tileMap.put(position, tile);
+    }
+
+    @Override
+    public GameMap build() {
+        final TileShapeLoader loader = new TileShapeLoader();
+        final WFCWrapper wfc = new WFCWrapperImpl(this.mapDimensions, loader.getAll());
+        this.tileMap.forEach((k, v) -> wfc.setStaticTile(k, v.getAdjacencies()));
+        wfc.runWFC();
+        for (int i = 0; i < mapDimensions.x(); i++) {
+            for (int j = 0; j < mapDimensions.y(); j++) {
+                final Pair<Integer, Integer> pos = new Pair<>(i, j);
+                tileMap.put(pos, wfc.getTileAt(pos));
+            }
+        }
+        return new GameMapImpl(mapDimensions, tileMap, playerTileMapPosition);
+    }
 }
