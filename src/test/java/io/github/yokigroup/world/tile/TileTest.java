8e30088936e382d0c0b68c1916fa2df33c828bc0
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-15 10:47:53 +0100

    Add id to tests

--- src/test/java/io/github/yokigroup/world/tile/TileTest.java
+++ src/test/java/io/github/yokigroup/world/tile/TileTest.java
@@ -79,11 +79,11 @@ class TileTest {
             }
         };
 
-        entityPool.addElement(new Altar(new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
-        entityPool.addElement(new Altar(new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
-        entityPool.addElement(new Altar(new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
-        entityPool.addElement(new Altar(new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
-        entityPool.addElement(new Altar(new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
+        entityPool.addElement(new Altar(0, new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
+        entityPool.addElement(new Altar(1, new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
+        entityPool.addElement(new Altar( 2, new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
+        entityPool.addElement(new Altar(3, new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
+        entityPool.addElement(new Altar(4, new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
         tile.addSpawnLocation(vector1);
         tile.addSpawnLocation(vector2);
         tile.addSpawnLocation(vector3);
