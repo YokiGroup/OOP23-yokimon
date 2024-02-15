a4f7f63f770c36fb2fba39877b24c06ee583b376
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-15 20:27:42 +0100

    Fix TileTest test implementations

--- src/test/java/io/github/yokigroup/world/tile/TileTest.java
+++ src/test/java/io/github/yokigroup/world/tile/TileTest.java
@@ -1,20 +1,10 @@
 package io.github.yokigroup.world.tile;
 
-import io.github.yokigroup.core.GameMessageHandler;
-import io.github.yokigroup.event.MessageHandler;
-import io.github.yokigroup.util.Vector2;
-import io.github.yokigroup.util.Vector2Impl;
-import io.github.yokigroup.util.WeightedPool;
-import io.github.yokigroup.util.WeightedPoolImpl;
-import io.github.yokigroup.world.entity.Altar;
-import io.github.yokigroup.world.entity.Entity;
-import io.github.yokigroup.world.entity.PositionImpl;
+import io.github.yokigroup.world.Direction;
 import io.github.yokigroup.world.entity.hitbox.CircularHitbox;
 import io.github.yokigroup.world.entity.hitbox.Hitbox;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -24,68 +14,35 @@ class TileTest {
     private static Hitbox hitbox1;
     private static Hitbox hitbox2;
     private static Hitbox hitbox3;
-    private static Vector2 vector1;
-    private static Vector2 vector2;
-    private static Vector2 vector3;
     private static Tile tile;
 
     @BeforeEach
     public void init() {
-        final double x1 = 1.0d;
-        final double y1 = 3.0d;
-        final double x2 = 2.0d;
-        final double y2 = 6.0d;
-        final double x3 = 3.0d;
-        final double y3 = 9.0d;
         final double r1 = 10.0d;
         final double r2 = 2.0d;
         final double r3 = 3.0d;
         hitbox1 = new CircularHitbox(r1);
         hitbox2 = new CircularHitbox(r2);
         hitbox3 = new CircularHitbox(r3);
-        tile = new TileImpl(0, new HashSet<>(), new HashSet<>());
-        vector1 = new Vector2Impl(x1, y1);
-        vector2 = new Vector2Impl(x2, y2);
-        vector3 = new Vector2Impl(x3, y3);
+        tile = new TileBuilderImpl()
+                .addHitbox(hitbox1)
+                .addHitbox(hitbox2)
+                .addHitbox(hitbox3)
+                .addAdjacency(Direction.UP)
+                .addAdjacency(Direction.DOWN)
+                .build(13);
     }
 
     @Test
     void tileGettersAndAdd() {
-        assertEquals(0, tile.getEntities().size());
-        assertEquals(0, tile.getHitboxes().size());
-        assertEquals(0, tile.getEntitySpawnLocations().size());
-        tile.addSpawnLocation(vector1);
-        assertEquals(1, tile.getEntitySpawnLocations().size());
-        tile.addSpawnLocation(vector2);
-        assertEquals(2, tile.getEntitySpawnLocations().size());
-        tile.addSpawnLocation(vector3);
-        assertEquals(3, tile.getEntitySpawnLocations().size());
-        tile.addHitbox(hitbox1);
-        assertEquals(1, tile.getHitboxes().size());
-        tile.addHitbox(hitbox2);
-        assertEquals(2, tile.getHitboxes().size());
-        tile.addHitbox(hitbox3);
+        assertEquals(13, tile.getId());
+        assertEquals(3, tile.getAdjacencies().size());
+        assertTrue(tile.getAdjacencies().contains(Direction.UP));
+        assertTrue(tile.getAdjacencies().contains(Direction.DOWN));
         assertEquals(3, tile.getHitboxes().size());
-    }
-
-    @Test
-    void spawnEntities() {
-        final WeightedPool<Entity> entityPool = new WeightedPoolImpl<>();
-        final MessageHandler mh = new GameMessageHandler();
-
-        entityPool.addElement(new Altar(0, new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
-        entityPool.addElement(new Altar(1, new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
-        entityPool.addElement(new Altar(2, new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
-        entityPool.addElement(new Altar(3, new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
-        entityPool.addElement(new Altar(4, new PositionImpl(new Vector2Impl(0, 0)), null, null, mh), 1.0f);
-        tile.addSpawnLocation(vector1);
-        tile.addSpawnLocation(vector2);
-        tile.addSpawnLocation(vector3);
-        tile.spawnEntities(entityPool);
-        assertEquals(3, tile.getEntities().size());
-        tile.getEntities()
-                .stream()
-                .map(e -> e.getPos().getPosition())
-                .forEach(v -> assertTrue(tile.getEntitySpawnLocations().contains(v)));
+        assertTrue(tile.getHitboxes().contains(hitbox1));
+        assertTrue(tile.getHitboxes().contains(hitbox2));
+        assertTrue(tile.getHitboxes().contains(hitbox3));
+        assertEquals(0, tile.getEntities().size());
     }
 }
