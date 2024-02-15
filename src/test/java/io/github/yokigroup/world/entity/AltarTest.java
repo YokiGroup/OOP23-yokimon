963783b36f87f900c7f7d88d00a34edfaf13f61c
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-02-15 13:16:01 +0100

    Change test

--- src/test/java/io/github/yokigroup/world/entity/AltarTest.java
+++ src/test/java/io/github/yokigroup/world/entity/AltarTest.java
@@ -7,16 +7,19 @@ import io.github.yokigroup.file.loader.YokimonLoader;
 import io.github.yokigroup.util.Vector2Impl;
 import io.github.yokigroup.world.entity.hitbox.Hitbox;
 import io.github.yokigroup.world.entity.hitbox.RectangularHitbox;
+import io.github.yokigroup.world.entity.people.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.print.attribute.standard.MediaSize;
 
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AltarTest {
     private static YokimonLoader loader;
-
+    private static MessageHandler messageHandler;
     @BeforeEach
     void setUp() {
         loader = new YokimonLoader();
@@ -29,7 +32,11 @@ class AltarTest {
         Position altarPos = new PositionImpl(new Vector2Impl(2, 0));
         Hitbox altarHitBox = new RectangularHitbox(altarPos.getPosition(), altarPos.getPosition());
         Hitbox playerHitBox = new RectangularHitbox(playerPos.getPosition(), new Vector2Impl(1, 0));
-        //Entity altar = new Altar(0, altarPos, altarHitBox, loader.load(1), );
+        Entity altar = new Altar(0, altarPos, altarHitBox, loader.load(1), messageHandler);
+        Entity player = new Player(1, playerPos, playerHitBox, Collections.emptyList(), messageHandler);
+
+
+
     }
 
     @Test
