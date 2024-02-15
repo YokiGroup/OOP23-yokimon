bfc0c078128fca8f4560ac39b4ab66401424eef8
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-15 18:08:51 +0100

    Add newline in Main

--- src/main/java/io/github/yokigroup/core/Main.java
+++ src/main/java/io/github/yokigroup/core/Main.java
@@ -6,6 +6,7 @@ import io.github.yokigroup.view.GameWindow;
 public class Main {
     private final GameWindow gameWindow = new App();
     private final GameLogicImpl model = new GameLogicImpl();
+
     public static void main(final String[] args){
         Main mainClass = new Main();
         mainClass.gameWindow.create(args);
