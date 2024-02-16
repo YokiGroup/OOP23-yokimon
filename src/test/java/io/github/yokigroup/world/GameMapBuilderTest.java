b3fb395d67d74978d5405a2720712845f7b8ce02
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-16 17:41:45 +0100

    Add GameMapBuilderTest implementation

--- src/test/java/io/github/yokigroup/world/GameMapBuilderTest.java
+++ src/test/java/io/github/yokigroup/world/GameMapBuilderTest.java
@@ -1,28 +1,28 @@
 package io.github.yokigroup.world;
 
+import io.github.yokigroup.core.GameMessageHandler;
+import io.github.yokigroup.event.MessageHandler;
+import io.github.yokigroup.file.loader.TileLoader;
+import io.github.yokigroup.util.Pair;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameMapBuilderTest {
 
     @Test
-    void setMapDimensions() {
-        // TODO: implement test
-    }
-
-    @Test
-    void setPlayerTileMapPosition() {
-        // TODO: implement test
-    }
-
-    @Test
-    void setStaticTileAt() {
-        // TODO: implement test
-    }
-
-    @Test
-    void build() {
-        // TODO: implement test
+    void gameMapBuilderTest() {
+        final MessageHandler messageHandler = new GameMessageHandler();
+        final TileLoader loader = new TileLoader();
+        final GameMapBuilder builderA = new GameMapBuilderImpl();
+        final GameMap mapA = builderA.build(messageHandler);
+        assertEquals(new Pair<>(0, 0), mapA.getPlayerTileMapPosition());
+        final GameMapBuilder builderB = new GameMapBuilderImpl()
+                .changeMapDimensions(new Pair<>(3, 3))
+                .putHomeTileAt(new Pair<>(1, 1))
+                .changePlayerTileMapPosition(new Pair<>(1, 1));
+        final GameMap mapB = builderB.build(messageHandler);
+        assertEquals(new Pair<>(1, 1), mapB.getPlayerTileMapPosition());
+        assertEquals(loader.getHomeTile().build(messageHandler), mapB.getTileAt(new Pair<>(1, 1)));
     }
 }
