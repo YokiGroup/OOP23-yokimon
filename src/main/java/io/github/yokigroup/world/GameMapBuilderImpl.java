eff7631e0a09d55c7393b7fcf5eb4b5047d61d7c
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-16 01:13:34 +0100

    Change GameMapBuilderImpl to use TileBuilder internally

--- src/main/java/io/github/yokigroup/world/GameMapBuilderImpl.java
+++ src/main/java/io/github/yokigroup/world/GameMapBuilderImpl.java
@@ -1,23 +1,26 @@
 package io.github.yokigroup.world;
 
 import io.github.yokigroup.event.MessageHandler;
+import io.github.yokigroup.file.loader.TileLoader;
 import io.github.yokigroup.file.loader.TileShapeLoader;
 import io.github.yokigroup.util.Pair;
 import io.github.yokigroup.world.gen.WFCWrapper;
 import io.github.yokigroup.world.gen.WFCWrapperImpl;
 import io.github.yokigroup.world.tile.Tile;
+import io.github.yokigroup.world.tile.TileBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Implementation of a builder class to create a GameMap object.
  */
 public class GameMapBuilderImpl implements GameMapBuilder {
-    private final Map<Pair<Integer, Integer>, Tile> tileMap;
+    private final Map<Pair<Integer, Integer>, TileBuilder> tileMap;
     private Pair<Integer, Integer> playerTileMapPosition;
     private Pair<Integer, Integer> mapDimensions;
-    private final TileShapeLoader loader = new TileShapeLoader();
+    private TileLoader tileLoader = new TileLoader();
 
     /**
      * Creates a 1x1 map with the player starting at 0, 0.
@@ -49,18 +52,28 @@ public class GameMapBuilderImpl implements GameMapBuilder {
     */
 
     @Override
+    public GameMapBuilder setHomeTileAt(Pair<Integer, Integer> position) {
+        this.tileMap.put(position, tileLoader.getHomeTile());
+        return this;
+    }
+
+    @Override
     public GameMap build(MessageHandler handler) {
+        final TileShapeLoader loader = new TileShapeLoader(tileLoader);
         final WFCWrapper wfc = new WFCWrapperImpl(this.mapDimensions, loader.getAll());
-        this.tileMap.forEach((k, v) -> wfc.setStaticTile(k, v.getAdjacencies()));
+        final Map<Pair<Integer, Integer>, Tile> builtTiles = tileMap.entrySet().stream()
+                .map(a -> new Pair<>(a.getKey(), a.getValue().build(handler)))
+                .collect(Collectors.toMap(Pair::x, Pair::y));
+        builtTiles.forEach((k, v) -> wfc.setStaticTile(k, v.getAdjacencies()));
         wfc.runWFC();
         for (int i = 0; i < mapDimensions.x(); i++) {
             for (int j = 0; j < mapDimensions.y(); j++) {
                 final Pair<Integer, Integer> pos = new Pair<>(i, j);
-                if (!tileMap.containsKey(pos)) {
-                    tileMap.put(pos, wfc.getTileAt(pos).build(handler));
+                if (!builtTiles.containsKey(pos)) {
+                    builtTiles.put(pos, wfc.getTileAt(pos).build(handler));
                 }
             }
         }
-        return new GameMapImpl(mapDimensions, tileMap, playerTileMapPosition);
+        return new GameMapImpl(mapDimensions, builtTiles, playerTileMapPosition);
     }
 }
