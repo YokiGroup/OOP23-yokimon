fc7a2f0c2257ad7ea77bb8259c835203ce0e70da
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-17 14:10:54 +0100

    Implement deep copying in PartySubmodule

--- src/main/java/io/github/yokigroup/event/submodule/PartySubmodule.java
+++ src/main/java/io/github/yokigroup/event/submodule/PartySubmodule.java
@@ -1,6 +1,7 @@
 package io.github.yokigroup.event.submodule;
 
 import io.github.yokigroup.battle.Yokimon;
+import io.github.yokigroup.battle.YokimonImpl;
 import io.github.yokigroup.event.MessageHandler;
 import io.github.yokigroup.event.submodule.abs.PartySubmoduleAbs;
 
@@ -8,10 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Submodule containing a party's information (yokimon's group).
+ * Submodule containing a yokimon's party information.
  * @author Giovanni Paone
  */
-public class PartySubmodule extends PartySubmoduleAbs {
+public final class PartySubmodule extends PartySubmoduleAbs {
     private List<Yokimon> yokimonList;
 
     /**
@@ -23,14 +24,12 @@ public class PartySubmodule extends PartySubmoduleAbs {
     }
 
     private List<Yokimon> deepCopyOf(final List<Yokimon> list) {
-        // TODO wait for a copy constructor of yokimon to be pushed upstream
-        //return List.copyOf(list.stream().map(y -> new YokimonImpl(y)));
-        return List.copyOf(list);
+        return list.stream().map(YokimonImpl::new).map(Yokimon.class::cast).toList();
     }
 
     @Override
     public void addYokimon(final Yokimon y) {
-        yokimonList.add(y);
+        yokimonList.add(new YokimonImpl(y));
     }
 
     @Override
@@ -45,7 +44,7 @@ public class PartySubmodule extends PartySubmoduleAbs {
 
     @Override
     public boolean removeYokimon(final Yokimon y) {
-        return yokimonList.remove(y);
+        return yokimonList.remove(new YokimonImpl(y));
     }
 
 }
