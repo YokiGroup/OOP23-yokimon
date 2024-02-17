84bc989419e5e245300989ec23ae27aacd3fe0eb
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-17 19:38:51 +0100

    Compliance of GameLogicImpl to static code analyzers

--- src/main/java/io/github/yokigroup/core/GameLogicImpl.java
+++ src/main/java/io/github/yokigroup/core/GameLogicImpl.java
@@ -2,11 +2,12 @@ package io.github.yokigroup.core;
 
 import io.github.yokigroup.event.MessageHandler;
 
+/**
+ * Instances and keeps the game model updated.
+ */
 public final class GameLogicImpl implements GameLogic {
     private final MessageHandler handler = new GameMessageHandler();
     private boolean running = true;
-
-
 
     @Override
     public void start() {
