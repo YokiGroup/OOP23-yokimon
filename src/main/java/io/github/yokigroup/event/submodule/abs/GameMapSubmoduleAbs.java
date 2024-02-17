551585725d43fadbf5b0a71ff424115d8a7e622d
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-17 13:49:00 +0100

    Compliance of GameMapSubmoduleAbs to static code analyzers

--- src/main/java/io/github/yokigroup/event/submodule/abs/GameMapSubmoduleAbs.java
+++ src/main/java/io/github/yokigroup/event/submodule/abs/GameMapSubmoduleAbs.java
@@ -9,10 +9,22 @@ import io.github.yokigroup.world.entity.hitbox.Hitbox;
 
 import java.util.Set;
 
+/**
+ * Abstract class of a submodule containing information about the game map.
+ * @see GameMap
+ * @author Giovanni Paone
+ */
 public abstract class GameMapSubmoduleAbs extends Submodule {
-    protected final Pair<Integer, Integer> MAP_DIM = new Pair<>(5, 5);
+    /**
+     * Dimensions of the map to instance.
+     */
+    protected static final Pair<Integer, Integer> MAP_DIM = new Pair<>(5, 5);
 
-    public GameMapSubmoduleAbs(MessageHandler handler) {
+
+    /**
+     * @param handler to init the submodule with
+     */
+    public GameMapSubmoduleAbs(final MessageHandler handler) {
         super(handler);
     }
 
@@ -28,11 +40,12 @@ public abstract class GameMapSubmoduleAbs extends Submodule {
 
     /**
      * Gets the entities contained in the Tile the player's currently on.
+     * @return set containing entities on the current tile
      */
     public abstract Set<Entity> getEntitiesOnCurrentTile();
 
     @Override
-    public void update() {
+    public final void update() {
         /*
         this function should query the player's position and consider whether to change Tile if the player is crossing
         the tile border.
