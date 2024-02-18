62514162d659f44489b2a64d7c575f38dcaac304
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-18 02:56:43 +0100

    Fix NullPointerException in DrawSetObserver

--- src/main/java/io/github/yokigroup/view/observer/DrawSetObserver.java
+++ src/main/java/io/github/yokigroup/view/observer/DrawSetObserver.java
@@ -10,13 +10,15 @@ import java.util.Set;
 public class DrawSetObserver implements EObserver<Set<SpriteData>> {
     private final Painter painter;
 
-    public DrawSetObserver(Painter painter) {
+    public DrawSetObserver(final Painter painter) {
         this.painter = painter;
     }
 
     @Override
-    public void update(PublisherImpl<Set<SpriteData>> publisher, Set<SpriteData> lastArg, Set<SpriteData> arg) {
-        lastArg.forEach(painter::removeFromPersistentDrawQueue);
+    public void update(final PublisherImpl<Set<SpriteData>> publisher, final Set<SpriteData> lastArg, final Set<SpriteData> arg) {
+        if (lastArg != null) {
+            lastArg.forEach(painter::removeFromPersistentDrawQueue);
+        }
         arg.forEach(painter::addToPersistentDrawQueue);
         painter.repaint();
     }
