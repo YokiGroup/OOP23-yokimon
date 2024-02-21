26c4251ea3d0444ed7b7a18a565f8a8986825f59
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-21 23:29:33 +0100

    Compliance of GameMap to static code analyzers

--- src/main/java/io/github/yokigroup/world/GameMap.java
+++ src/main/java/io/github/yokigroup/world/GameMap.java
@@ -1,10 +1,7 @@
 package io.github.yokigroup.world;
 
 import io.github.yokigroup.util.Pair;
-import io.github.yokigroup.world.entity.people.Enemy;
 import io.github.yokigroup.world.tile.Tile;
-
-import java.util.Set;
 
 /**
  * A map containing tiles the player can traverse on.
