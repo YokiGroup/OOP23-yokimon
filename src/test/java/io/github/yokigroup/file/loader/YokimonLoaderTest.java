6482386e61d63300ad3c71b96503098dde08b55d
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-22 00:24:21 +0100

    Compliance of YokimonLoaderTest to static code analyzers.

--- src/test/java/io/github/yokigroup/file/loader/YokimonLoaderTest.java
+++ src/test/java/io/github/yokigroup/file/loader/YokimonLoaderTest.java
@@ -4,12 +4,11 @@ import io.github.yokigroup.battle.Color;
 import io.github.yokigroup.battle.Yokimon;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class YokimonLoaderTest {
+    private static final int ATK_ID1 = 18;
     private static YokimonLoader loader;
 
     @BeforeEach
@@ -25,7 +24,7 @@ class YokimonLoaderTest {
         assertEquals("Tengu", y.getName());
         assertEquals(Yokimon.GrowthRate.MEDIUM, y.getGrowRate());
         assertEquals(Color.RED, y.getYokimonColor());
-        assertEquals(attackLoader.load(18), y.getAttacks().get(0));
+        assertEquals(attackLoader.load(ATK_ID1), y.getAttacks().get(0));
         // FIXME check the validity of all yokimon fields (with reflection if need be)
 
     }
