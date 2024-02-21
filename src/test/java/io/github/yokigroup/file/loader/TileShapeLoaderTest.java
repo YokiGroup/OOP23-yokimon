13fada2107b510a5a59a80770cee78a1762b61e8
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-22 00:11:30 +0100

    Compliance of TileShapeLoaderTest to static code analyzers.

--- src/test/java/io/github/yokigroup/file/loader/TileShapeLoaderTest.java
+++ src/test/java/io/github/yokigroup/file/loader/TileShapeLoaderTest.java
@@ -17,9 +17,21 @@ import org.junit.jupiter.api.Test;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 class TileShapeLoaderTest {
+    private static final double VEC2_X1 = 10.0;
+    private static final double VEC2_Y1 = 30.0;
+    private static final double VEC2_X2 = 10.0;
+    private static final double VEC2_Y2 = 5.0;
+    private static final double VEC2_X3 = 5.0;
+    private static final double VEC2_Y3 = 5.0;
+    private static final double VEC2_X4 = 30.0;
+    private static final double VEC2_Y4 = 5.0;
+    private static final double RAD = 5.0;
+
     private TileShape loadedTileShape;
     private Tile loadedTile;
 
@@ -29,35 +41,40 @@ class TileShapeLoaderTest {
         TileShapeLoader tileShapeLoader = new TileShapeLoader();
         var tileShapeSet = tileShapeLoader.getAll();
 
+        //FIXME
         assertEquals(2, tileShapeSet.size());
-        for(var t: tileShapeSet) {
-            if(t.getTiles().size() == 2) loadedTileShape = t;
+        for (var t: tileShapeSet) {
+            if (t.getTiles().size() == 2) {
+                loadedTileShape = t;
+            }
         }
         assertNotNull(loadedTileShape);
 
         assertEquals(2, loadedTileShape.getTiles().size());
-        for(var t: loadedTileShape.getTiles().getEntries()){
+        for (var t: loadedTileShape.getTiles().getEntries()){
             Tile builtTile = t.build(handler);
-            if(builtTile.getHitboxes().size() == 2) loadedTile = builtTile;
+            if (builtTile.getHitboxes().size() == 2) {
+                loadedTile = builtTile;
+            }
         }
         assertNotNull(loadedTile);
     }
 
     @Test
     void spawnsTest() {
-        Set<Vector2> spawns = Set.of(new Vector2Impl(10.0, 30.0));
+        Set<Vector2> spawns = Set.of(new Vector2Impl(VEC2_X1, VEC2_Y1));
         assertEquals(spawns, loadedTile.getEntities().stream().map(Entity::getPos).collect(Collectors.toSet()));
     }
 
     @Test
     void hitboxesTest() {
         Hitbox rectHitbox = new RectangularHitbox(
-                new Vector2Impl(10.0, 5.0),
-                new Vector2Impl(5.0, 5.0)
+                new Vector2Impl(VEC2_X2, VEC2_Y2),
+                new Vector2Impl(VEC2_X3, VEC2_Y3)
         );
         Hitbox circleHitbox = new CircularHitbox(
-                new Vector2Impl(30.0, 5.0),
-                5.0
+                new Vector2Impl(VEC2_X4, VEC2_Y4),
+                RAD
         );
         assertEquals(
                 Set.of(rectHitbox, circleHitbox),
