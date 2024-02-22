efce2aa4278c6450b5db2fec2f619c7653e33ccb
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-22 10:52:33 +0100

    Compliance of GameWindow to static code analyzers.

--- src/main/java/io/github/yokigroup/view/GameWindow.java
+++ src/main/java/io/github/yokigroup/view/GameWindow.java
@@ -1,5 +1,13 @@
 package io.github.yokigroup.view;
 
+/**
+ * The game window.
+ */
 public interface GameWindow {
+
+    /**
+     * Create a new game window.
+     * @param args ...
+     */
     void create(String[] args);
 }
