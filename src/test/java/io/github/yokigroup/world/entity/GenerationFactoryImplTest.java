0a2c2d09e1f4a9069646cb4052cb4ef6d68ec691
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-22 10:19:06 +0100

    Compliance of GenerationFactoryImplTest to static code analyzers.

--- src/test/java/io/github/yokigroup/world/entity/GenerationFactoryImplTest.java
+++ src/test/java/io/github/yokigroup/world/entity/GenerationFactoryImplTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class GenerationFactoryImplTest {
