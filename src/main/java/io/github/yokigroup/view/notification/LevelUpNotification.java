bed659ba196a6f2c1bf2d02f8051eb530c3744a3
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-22 10:52:41 +0100

    Compliance of LevelUpNotification to static code analyzers.

--- src/main/java/io/github/yokigroup/view/notification/LevelUpNotification.java
+++ src/main/java/io/github/yokigroup/view/notification/LevelUpNotification.java
@@ -1,6 +1,18 @@
 package io.github.yokigroup.view.notification;
 
+/**
+ * Notification system for Level-up events.
+ */
 public interface LevelUpNotification extends Notification{
+    /**
+     * The new level of the Yokimon.
+     * @return the new level.
+     */
     int levelReached();
+
+    /**
+     * Name of the Yokimon who must level-up.
+     * @return the name.
+     */
     String yokimonName();
 }
