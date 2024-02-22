6f13db9fc664fffa660110d023ba7c2f6712e327
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-22 10:30:41 +0100

    Compliance of InputSubmoduleAbs to static code analyzers.

--- src/main/java/io/github/yokigroup/event/submodule/abs/InputSubmoduleAbs.java
+++ src/main/java/io/github/yokigroup/event/submodule/abs/InputSubmoduleAbs.java
@@ -3,15 +3,27 @@ package io.github.yokigroup.event.submodule.abs;
 import io.github.yokigroup.event.MessageHandler;
 import io.github.yokigroup.view.render.observer.ModelObserver;
 
+/**
+ * Input Submodules.
+ */
 public abstract class InputSubmoduleAbs extends Submodule {
     /**
-     * @param handler         MessageHandler to call in order to query other submodules.
-     * @param ignoredModelObs
+     * @param handler MessageHandler to call in order to query other submodules.
+     * @param ignoredModelObs ...
      */
-    public InputSubmoduleAbs(MessageHandler handler, ModelObserver ignoredModelObs) {
+    public InputSubmoduleAbs(final MessageHandler handler, final ModelObserver ignoredModelObs) {
         super(handler, ignoredModelObs);
     }
 
+    /**
+     * ...
+     * @param keyText ...
+     */
     public abstract void registerKeyPress(String keyText);
+
+    /**
+     * ...
+     * @param keyText ...
+     */
     public abstract void registerKeyRelease(String keyText);
 }
