f66677f0493a997bb64f21cd7d29c07b749cfff3
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-22 10:52:56 +0100

    Compliance of NewYokimonNotification to static code analyzers.

--- src/main/java/io/github/yokigroup/view/notification/NewYokimonNotification.java
+++ src/main/java/io/github/yokigroup/view/notification/NewYokimonNotification.java
@@ -1,5 +1,13 @@
 package io.github.yokigroup.view.notification;
 
+/**
+ * Notification system for when a new Yokimon is found.
+ */
 public interface NewYokimonNotification extends Notification {
+
+    /**
+     * The name of the new Yokimon found.
+     * @return the name of the Yokimon.
+     */
     String yokimonName();
 }
