c8a23f6f1c6aaee7117a29ae87cdfdcee5908339
Marilia Merendi <marilia.merendi@studio.unibo.it>
2024-02-22 10:19:27 +0100

    Compliance of Main to static code analyzers.

--- src/main/java/io/github/yokigroup/core/Main.java
+++ src/main/java/io/github/yokigroup/core/Main.java
@@ -3,11 +3,18 @@ package io.github.yokigroup.core;
 import io.github.yokigroup.view.App;
 import io.github.yokigroup.view.GameWindow;
 
+/**
+ * The main class to run the program.
+ */
 public class Main {
     private final GameWindow gameWindow = new App();
     //private final GameLogicImpl model = new GameLogicImpl();
 
-    public static void main(final String[] args){
+    /**
+     * Launch the game.
+     * @param args ...
+     */
+    public static void main(final String[] args) {
         Main mainClass = new Main();
         mainClass.gameWindow.create(args);
     }
