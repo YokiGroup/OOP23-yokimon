746b52bf173dde5c55f4f5a95ffc58407119629f
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-24 16:55:11 +0100

    Add javadoc for Notification

--- src/main/java/io/github/yokigroup/view/notification/Notification.java
+++ src/main/java/io/github/yokigroup/view/notification/Notification.java
@@ -1,9 +1,15 @@
 package io.github.yokigroup.view.notification;
 
 /**
- * Notification system.
+ * Notification system. Notifies the view of events that need to be comunicated to the player, along with their details.
  */
 public interface Notification {
 
+    /**
+     * given a {@param visitor} extracts the message to be displayed to the player.
+     * @param visitor {@link NotificationVisitor} to process and generate a string representing the notification.
+     * @return The event string to be displayed in the view
+     * @see NotificationVisitor
+     */
     String getMessage(NotificationVisitor visitor);
 }
