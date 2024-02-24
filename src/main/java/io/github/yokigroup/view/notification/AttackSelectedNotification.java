fbdd61ce47fa56f297881a7e5614407439f004df
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-24 16:50:25 +0100

    Add javadoc for AttackSelectedNotification

--- src/main/java/io/github/yokigroup/view/notification/AttackSelectedNotification.java
+++ src/main/java/io/github/yokigroup/view/notification/AttackSelectedNotification.java
@@ -2,6 +2,12 @@ package io.github.yokigroup.view.notification;
 
 import io.github.yokigroup.battle.attack.Attack;
 
+/**
+ * Notification used for notifying what attack has been selected by the player's yokimon.
+ */
 public interface AttackSelectedNotification extends Notification {
+    /**
+     * @return the selected attack
+     */
     Attack getSelectedAttack();
 }
