9adf9160cc4dfcaa1788a0b0c5d9b6b004af1de8
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-24 16:51:10 +0100

    Add javadoc for AttackSelectedNotitication

--- src/main/java/io/github/yokigroup/view/notification/AttackSelectedNotificationImpl.java
+++ src/main/java/io/github/yokigroup/view/notification/AttackSelectedNotificationImpl.java
@@ -2,9 +2,15 @@ package io.github.yokigroup.view.notification;
 
 import io.github.yokigroup.battle.attack.Attack;
 
+/**
+ * Implementation of {@link AttackSelectedNotification}.
+ */
 public final class AttackSelectedNotificationImpl implements AttackSelectedNotification {
     private final Attack selectedAttack;
 
+    /**
+     * @param selectedAttack attack to notify the selection of
+     */
     public AttackSelectedNotificationImpl(final Attack selectedAttack) {
         this.selectedAttack = selectedAttack;
     }
