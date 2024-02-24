97dd24cbdfe00f61a17107714a598a8bc43c6e20
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-24 16:53:01 +0100

    Add javadoc to DeathNotificationImpl

--- src/main/java/io/github/yokigroup/view/notification/DeathNotificationImpl.java
+++ src/main/java/io/github/yokigroup/view/notification/DeathNotificationImpl.java
@@ -1,8 +1,14 @@
 package io.github.yokigroup.view.notification;
 
+/**
+ * Implementation of {@link DeathNotification}.
+ */
 public final class DeathNotificationImpl implements DeathNotification {
     private final Cause causeOfDeath;
 
+    /**
+     * @param causeOfDeath cause of death to notify to the view
+     */
     public DeathNotificationImpl(final Cause causeOfDeath) {
         this.causeOfDeath = causeOfDeath;
     }
