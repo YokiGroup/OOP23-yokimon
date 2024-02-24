fade224e10d81ba2d47d080fcd8f8bcc53cf8f4e
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-02-24 14:53:52 +0100

    Compliance of FightSubmodule to static code analyzers

--- src/main/java/io/github/yokigroup/view/render/arranger/BattleArranger.java
+++
@@ -1,9 +0,0 @@
-package io.github.yokigroup.view.render.arranger;
-
-import io.github.yokigroup.view.render.drawable.SpriteData;
-import io.github.yokigroup.event.observer.Publisher;
-
-public interface BattleArranger {
-    void setPlayerYokimon(Publisher<SpriteData> playerYokimonSpritePub);
-    void setEnemyYokimon(Publisher<SpriteData> enemyYokimonSpritePub);
-}
