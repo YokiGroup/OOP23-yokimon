95343748621245b53401decc61b5932dc554111f
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-24 16:52:18 +0100

    Add javadoc for DeathNotification

--- src/main/java/io/github/yokigroup/view/notification/DeathNotification.java
+++ src/main/java/io/github/yokigroup/view/notification/DeathNotification.java
@@ -1,9 +1,25 @@
 package io.github.yokigroup.view.notification;
 
+/**
+ * Notifies the cause of death of the player.
+ */
 public interface DeathNotification extends Notification {
+    /**
+     * Cause of death.
+     */
     enum Cause {
+        /**
+         * The player commenced a fight without carrying any yokimons in their party.
+         */
         UNPREPARED_FOR_BATTLE,
+        /**
+         * The player finished their stock of yokimons during a battle.
+         */
         DEFEATED_IN_BATTLE
     }
+
+    /**
+     * @return cause of death of the player
+     */
     Cause getCause();
 }
