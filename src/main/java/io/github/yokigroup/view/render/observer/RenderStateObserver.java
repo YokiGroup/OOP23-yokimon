d444f6411035cd4d2a010c7062a1885215233326
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-24 19:27:01 +0100

    Add javadoc for RenderStateObserver

--- src/main/java/io/github/yokigroup/view/render/observer/RenderStateObserver.java
+++ src/main/java/io/github/yokigroup/view/render/observer/RenderStateObserver.java
@@ -3,6 +3,9 @@ package io.github.yokigroup.view.render.observer;
 import io.github.yokigroup.view.render.painter.Painter;
 import io.github.yokigroup.view.render.RenderState;
 
+/**
+ * Observer whose exclusive function is to listen for and update the painter in case of {@link RenderState} changes.
+ */
 public final class RenderStateObserver extends ViewObserver<RenderState> {
 
     /**
