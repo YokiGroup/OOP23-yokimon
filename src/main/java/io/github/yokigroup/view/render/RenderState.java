27a875fd6489fbe5ed9527f3ab7999aaa2d25893
Giovanni Paone <giovanni.paone@studio.unibo.it>
2024-02-24 19:54:59 +0100

    Add javadoc to RenderState

--- src/main/java/io/github/yokigroup/view/render/RenderState.java
+++ src/main/java/io/github/yokigroup/view/render/RenderState.java
@@ -1,5 +1,8 @@
 package io.github.yokigroup.view.render;
 
+/**
+ * Enum representing the various states the program can be in. This state influences what is displayed on screen.
+ */
 public enum RenderState {
     /**
      * World render state.
